40daa82c8014c2bef09a48c4644926b6884c195b
Jae Lim <jaelim@microsoft.com>
2016-07-08 15:20:04 -0700

    Adding database feature to StorageHelperTest and improving code coverage.

--- base/src/main/java/avalanche/base/utils/StorageHelper.java
+++ base/src/main/java/avalanche/base/utils/StorageHelper.java
@@ -1,11 +1,16 @@
 package avalanche.base.utils;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.support.annotation.IntRange;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.text.TextUtils;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.Closeable;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -17,6 +22,7 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
+import java.util.Iterator;
 import java.util.Set;
 
 /**
@@ -30,6 +36,11 @@ public final class StorageHelper {
     private static final String PREFERENCES_NAME = "AvalancheSDK";
 
     /**
+     * Application context instance.
+     */
+    private static Context sContext;
+
+    /**
      * Android SharedPreferences instance.
      */
     private static SharedPreferences sSharedPreferences;
@@ -40,7 +51,8 @@ public final class StorageHelper {
      * @param context The context of the application.
      */
     public static void initialize(Context context) {
-        sSharedPreferences = context.getSharedPreferences(PREFERENCES_NAME, Context.MODE_PRIVATE);
+        sContext = context;
+        sSharedPreferences = sContext.getSharedPreferences(PREFERENCES_NAME, Context.MODE_PRIVATE);
     }
 
     /**
@@ -161,6 +173,9 @@ public final class StorageHelper {
         }
     }
 
+    /**
+     * InternalStorage Helper class
+     */
     public final static class InternalStorage {
         /**
          * Reads contents from the file.
@@ -168,7 +183,7 @@ public final class StorageHelper {
          * @param filename The name of the file.
          * @return The contents of the file.
          */
-        public static String read(String filename) {
+        public static String read(@NonNull String filename) {
             return read(new File(filename));
         }
 
@@ -178,7 +193,7 @@ public final class StorageHelper {
          * @param file The file instance.
          * @return The contents of the file.
          */
-        public static String read(File file) {
+        public static String read(@NonNull File file) {
             StringBuilder contents = new StringBuilder();
             BufferedReader reader = null;
             try {
@@ -212,7 +227,7 @@ public final class StorageHelper {
          * @param contents The content to be written to the file.
          * @throws IOException
          */
-        public static void write(String filename, String contents) throws IOException {
+        public static void write(@NonNull String filename, String contents) throws IOException {
             write(new File(filename), contents);
         }
 
@@ -223,8 +238,8 @@ public final class StorageHelper {
          * @param contents The content to be written to the file.
          * @throws IOException
          */
-        public static void write(File file, String contents) throws IOException {
-            if (TextUtils.isEmpty(contents) && TextUtils.getTrimmedLength(contents) > 0) {
+        public static void write(@NonNull File file, String contents) throws IOException {
+            if (TextUtils.isEmpty(contents) || TextUtils.getTrimmedLength(contents) <= 0) {
                 return;
             }
 
@@ -249,7 +264,8 @@ public final class StorageHelper {
          * @throws IOException
          * @throws ClassNotFoundException
          */
-        public static <T extends Serializable> T readObject(File file, Class<T> clazz) throws IOException, ClassNotFoundException {
+        public static <T extends Serializable> T readObject(@NonNull File file, @NonNull Class<T> clazz)
+                throws IOException, ClassNotFoundException {
             ObjectInputStream inputStream = null;
             T object;
             try {
@@ -269,7 +285,7 @@ public final class StorageHelper {
          * @param object The object to be written to the file.
          * @throws IOException
          */
-        public static <T extends Serializable> void writeObject(File file, T object) throws IOException {
+        public static <T extends Serializable> void writeObject(@NonNull File file, T object) throws IOException {
             ObjectOutputStream outputStream = null;
             try {
                 outputStream = new ObjectOutputStream(new FileOutputStream(file));
@@ -287,7 +303,7 @@ public final class StorageHelper {
          * @param filter The filter to match names against, may be {@code null}.
          * @return An array of filename that doesn't include paths.
          */
-        public static String[] getFilenames(String path, FilenameFilter filter) {
+        public static String[] getFilenames(@NonNull String path, FilenameFilter filter) {
             File dir = new File(path);
             if (dir.exists()) {
                 return dir.list(filter);
@@ -303,7 +319,7 @@ public final class StorageHelper {
          * @param filter The filter to match names against, may be {@code null}.
          * @return A file instance, may be {@code null} if the directory is empty.
          */
-        public static File lastModifiedFile(String path, FilenameFilter filter) {
+        public static File lastModifiedFile(@NonNull String path, FilenameFilter filter) {
             File dir = new File(path);
             if (dir.exists()) {
                 File[] files = dir.listFiles(filter);
@@ -326,23 +342,235 @@ public final class StorageHelper {
         }
 
         /**
-         * Deletes a file with the given filename.
+         * Deletes a file or directory with the given name.
          *
-         * @param filename The name of the file.
-         * @return {@code true} if this file was deleted, {@code false} otherwise.
+         * @param name The name of the file or directory.
+         * @return {@code true} if it was deleted, {@code false} otherwise.
          */
-        public static boolean delete(String filename) {
-            return delete(new File(filename));
+        public static boolean delete(@NonNull String name) {
+            return delete(new File(name));
         }
 
         /**
          * Deletes the file.
          *
          * @param file The file instance.
-         * @return {@code true} if this file was deleted, {@code false} otherwise.
+         * @return {@code true} if it was deleted, {@code false} otherwise.
          */
-        public static boolean delete(File file) {
+        public static boolean delete(@NonNull File file) {
             return file.delete();
+        }
+
+        /**
+         * Creates the directory if it does not already exist.
+         *
+         * @param path An absolute path for directory.
+         */
+        public static void mkdir(@NonNull String path) {
+            File dir = new File(path);
+            dir.mkdirs();
+        }
+    }
+
+    /**
+     * DatabaseStorage Helper class
+     */
+    public final static class DatabaseStorage implements Closeable {
+
+        /**
+         * DatabaseManager instance.
+         */
+        private final DatabaseManager mDatabaseManager;
+
+        /**
+         * Private constructor.
+         *
+         * @param databaseManager An instance of {@code DatabaseManager}.
+         */
+        private DatabaseStorage(@NonNull DatabaseManager databaseManager) {
+            mDatabaseManager = databaseManager;
+        }
+
+        /**
+         * Gets a new instance of {@code DatabaseManager}.
+         *
+         * @param database The database name.
+         * @param table    The table name.
+         * @param version  The version.
+         * @param schema   The schema of the database. If the database has tables more than 1,
+         *                 the schema should contain schemas for all the tables.
+         * @param listener The error listener
+         * @return
+         */
+        public static DatabaseStorage getDatabaseStorage(@NonNull String database,
+                                                         @NonNull String table,
+                                                         @IntRange(from = 1) int version,
+                                                         @NonNull ContentValues schema,
+                                                         final DatabaseErrorListener listener) {
+            return getDatabaseStorage(database, table, version, schema, 0, listener);
+        }
+
+        /**
+         * Gets a new instance of {@code DatabaseManager}.
+         *
+         * @param database   The database name.
+         * @param table      The table name.
+         * @param version    The version.
+         * @param schema     The schema of the database. If the database has tables more than 1,
+         *                   the schema should contain schemas for all the tables.
+         * @param maxRecords The maximum number of records allowed in the table.
+         * @param listener   The error listener
+         * @return
+         */
+        public static DatabaseStorage getDatabaseStorage(@NonNull String database,
+                                                         @NonNull String table,
+                                                         @IntRange(from = 1) int version,
+                                                         @NonNull ContentValues schema,
+                                                         @IntRange(from = 0) int maxRecords,
+                                                         final DatabaseErrorListener listener) {
+            return new DatabaseStorage(new DatabaseManager(sContext, database, table, version, schema, maxRecords, new DatabaseManager.ErrorListener() {
+                @Override
+                public void onError(String operation, RuntimeException e) {
+                    listener.onError(operation, e);
+                }
+            }));
+        }
+
+        /**
+         * Stores the entry to the table.
+         *
+         * @param values The entry to be stored.
+         * @return A database identifier
+         */
+        public Long put(@NonNull ContentValues values) {
+            return mDatabaseManager.put(values);
+        }
+
+        /**
+         * Updates the entry for the identifier.
+         *
+         * @param id     The existing database identifier.
+         * @param values The entry to be updated.
+         * @return true if the values updated successfully, false otherwise.
+         */
+        public boolean update(@IntRange(from = 0) long id, @NonNull ContentValues values) {
+            return mDatabaseManager.update(id, values);
+        }
+
+        /**
+         * Deletes the entry by the identifier from the database.
+         *
+         * @param id The database identifier.
+         */
+        public void delete(@IntRange(from = 0) long id) {
+            mDatabaseManager.delete(id);
+        }
+
+        /**
+         * Gets the entry by the identifier.
+         *
+         * @param id The database identifier.
+         * @return An entry for the identifier or null if not found.
+         */
+        public ContentValues get(@IntRange(from = 0) long id) {
+            return mDatabaseManager.get(id);
+        }
+
+        /**
+         * Gets the entry that matches key == value.
+         *
+         * @param key   The optional key for query.
+         * @param value The optional value for query.
+         * @return A matching entry.
+         */
+        public ContentValues get(@Nullable String key, @Nullable Object value) {
+            return mDatabaseManager.get(key, value);
+        }
+
+        /**
+         * Gets a scanner to iterate all values.
+         *
+         * @return A scanner to iterate all values.
+         */
+        public DatabaseScanner getScanner() {
+            return getScanner(null, null);
+        }
+
+        /**
+         * Gets a scanner to iterate all values those match key == value.
+         *
+         * @param key   The optional key for query.
+         * @param value The optional value for query.
+         * @return A scanner to iterate all values.
+         */
+        public DatabaseScanner getScanner(@Nullable String key, @Nullable Object value) {
+            return new DatabaseScanner(mDatabaseManager.getScanner(key, value));
+        }
+
+        /**
+         * Clears the table in the database.
+         */
+        public void clear() {
+            mDatabaseManager.clear();
+        }
+
+        /**
+         * Closes database and clean up in-memory database.
+         *
+         * @throws IOException
+         */
+        @Override
+        public void close() throws IOException {
+            mDatabaseManager.close();
+        }
+
+        /**
+         * Gets the count of records in the table. This is TEST PURPOSE ONLY.
+         *
+         * @return The number of records in the table.
+         */
+        protected long getRowCount() {
+            return mDatabaseManager.getRowCount();
+        }
+
+        /**
+         * Gets an array of column names in the table. This is TEST PURPOSE ONLY.
+         *
+         * @return An array of column names.
+         */
+        protected String[] getColumnNames() {
+            return mDatabaseManager.getCursor(null, null).getColumnNames();
+        }
+
+        /**
+         * Listener specification, each callback is called only once per instance
+         */
+        public interface DatabaseErrorListener {
+            /**
+             * Notifies an exception
+             */
+            void onError(String operation, RuntimeException e);
+        }
+
+        /**
+         * Database scanner to iterate values.
+         */
+        public static class DatabaseScanner implements Iterable<ContentValues>, Closeable {
+            private DatabaseManager.Scanner mScanner;
+
+            private DatabaseScanner(DatabaseManager.Scanner scanner) {
+                mScanner = scanner;
+            }
+
+            @Override
+            public void close() {
+                mScanner.close();
+            }
+
+            @Override
+            public Iterator<ContentValues> iterator() {
+                return mScanner.iterator();
+            }
         }
     }
 }
