10f0a2bce69e269f56fa746fe1cd27d169d3cdcc
Guillaume Perrot <guperrot@microsoft.com>
2016-07-08 18:31:03 -0700

    Javadoc/refactoring for Analytics module

--- analytics/src/main/java/avalanche/analytics/Analytics.java
+++ analytics/src/main/java/avalanche/analytics/Analytics.java
@@ -1,6 +1,7 @@
 package avalanche.analytics;
 
 import android.app.Activity;
+import android.support.annotation.NonNull;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -18,12 +19,29 @@ import avalanche.base.ingestion.models.json.LogFactory;
 
 import static avalanche.base.channel.DefaultAvalancheChannel.ANALYTICS_GROUP;
 
+/**
+ * Analytics feature.
+ */
 public class Analytics extends AbstractAvalancheFeature {
 
-    private static Analytics sharedInstance = null;
+    /**
+     * Activity suffix to exclude from generated page names.
+     */
+    private static final String ACTIVITY_SUFFIX = "Activity";
 
+    /**
+     * Shared instance.
+     */
+    private static Analytics sInstance = null;
+
+    /**
+     * Log factories managed by this module.
+     */
     private final Map<String, LogFactory> mFactories;
 
+    /**
+     * Init.
+     */
     protected Analytics() {
         mFactories = new HashMap<>();
         mFactories.put(EndSessionLog.TYPE, new EndSessionLogFactory());
@@ -31,16 +49,27 @@ public class Analytics extends AbstractAvalancheFeature {
         mFactories.put(EventLog.TYPE, new EventLogFactory());
     }
 
+    /**
+     * Get shared instance.
+     *
+     * @return shared instance.
+     */
     public static Analytics getInstance() {
-        if (sharedInstance == null) {
-            sharedInstance = new Analytics();
+        if (sInstance == null) {
+            sInstance = new Analytics();
         }
-        return sharedInstance;
+        return sInstance;
     }
 
-    private static String getDefaultPageName(Class<?> activityClass) {
+    /**
+     * Generate a page name for an activity.
+     *
+     * @param activityClass activity class.
+     * @return page name.
+     */
+    private static String generatePageName(Class<?> activityClass) {
         String name = activityClass.getSimpleName();
-        String suffix = "Activity";
+        String suffix = ACTIVITY_SUFFIX;
         if (name.endsWith(suffix) && name.length() > suffix.length())
             return name.substring(0, name.length() - suffix.length());
         else
@@ -54,17 +83,30 @@ public class Analytics extends AbstractAvalancheFeature {
 
     @Override
     public void onActivityResumed(Activity activity) {
-        sendPage(getDefaultPageName(activity.getClass()), null);
+        // TODO add a way to customize the automatic page behavior
+        sendPage(generatePageName(activity.getClass()), null);
     }
 
-    public void sendPage(String name, Map<String, String> properties) {
+    /**
+     * Send a page.
+     *
+     * @param name       page name.
+     * @param properties optional properties.
+     */
+    public void sendPage(@NonNull String name, Map<String, String> properties) {
         PageLog pageLog = new PageLog();
         pageLog.setName(name);
         pageLog.setProperties(properties);
         send(pageLog);
     }
 
-    public void sendEvent(String name, Map<String, String> properties) {
+    /**
+     * Send an event.
+     *
+     * @param name       event name.
+     * @param properties optional properties.
+     */
+    public void sendEvent(@NonNull String name, Map<String, String> properties) {
         EventLog eventLog = new EventLog();
         eventLog.setId(UUID.randomUUID());
         eventLog.setName(name);
@@ -72,6 +114,11 @@ public class Analytics extends AbstractAvalancheFeature {
         send(eventLog);
     }
 
+    /**
+     * Send log to channel.
+     *
+     * @param log log to send.
+     */
     private void send(Log log) {
         mChannel.enqueue(log, ANALYTICS_GROUP);
     }
