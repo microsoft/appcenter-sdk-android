987c444f31c772f4a739d56275c2f2e08f867e86
Matthias Wenz <matwen@microsoft.com>
2016-07-22 12:47:34 +0200

    Wire up uncaught exception handler with error reporting

--- sdk/errors/src/main/java/avalanche/errors/UncaughtExceptionHandler.java
+++ sdk/errors/src/main/java/avalanche/errors/UncaughtExceptionHandler.java
@@ -2,7 +2,11 @@ package avalanche.errors;
 
 import android.os.Process;
 
+import java.util.Set;
+
 import avalanche.core.Constants;
+import avalanche.core.utils.UUIDUtils;
+import avalanche.errors.ingestion.models.ErrorLog;
 
 public class UncaughtExceptionHandler implements Thread.UncaughtExceptionHandler {
 
@@ -10,7 +14,7 @@ public class UncaughtExceptionHandler implements Thread.UncaughtExceptionHandler
     private Thread.UncaughtExceptionHandler mDefaultUncaughtExceptionHandler;
 
     public UncaughtExceptionHandler() {
-
+        register();
     }
 
     @Override
@@ -30,6 +34,26 @@ public class UncaughtExceptionHandler implements Thread.UncaughtExceptionHandler
     }
 
     private void saveException(Thread thread, Throwable exception) {
+        ErrorLog errorLog = new ErrorLog();
+        errorLog.setId(UUIDUtils.randomUUID());
+        /*
+            - Parent process information intentionally left blank
+            - applicationPath, exceptionType, exceptionCode, exceptionAddress intentionally left blank
+            (Android does not provide any of this information)
+         */
+
+        // TODO Provide process name, probably hacky, maybe as per http://stackoverflow.com/questions/8542326/android-how-to-get-the-processname-or-packagename-by-using-pid
+
+        errorLog.setProcessId(Process.myPid());
+        errorLog.setCrashThread((int) thread.getId()); // TODO maybe redefine model value to be of type long
+        errorLog.setAppLaunchTOffset(System.currentTimeMillis() - ErrorReporting.getInstance().getInitializeTimestamp());
+        errorLog.setExceptionReason(exception.getMessage());
+        errorLog.setFatal(true);
+
+        Set<Thread> allLiveThreads = Thread.getAllStackTraces().keySet();
+        for (Thread t : allLiveThreads) {
+
+        }
 
     }
 
