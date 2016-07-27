2fb3e0a25699719fb8f0065893c960d99d444e04
Jae Lim <jaelim@microsoft.com>
2016-07-27 16:58:22 -0700

    Add a Channel listener to get callbacks when Channel completed its work for feature.

--- sdk/core/src/main/java/avalanche/core/channel/AvalancheChannel.java
+++ sdk/core/src/main/java/avalanche/core/channel/AvalancheChannel.java
@@ -9,13 +9,15 @@ import avalanche.core.ingestion.models.Log;
  */
 public interface AvalancheChannel {
 
+    void addGroup(String groupName, int maxLogsPerBatch, int batchTimeInterval, int maxParallelBatches, Listener listener);
+
     /**
      * Add Log to queue to be persisted and sent.
      *
      * @param log       the Log to be enqueued
      * @param queueName the queue to use
      */
-    void enqueue(@NonNull Log log, @NonNull @GroupNameDef String queueName);
+    void enqueue(@NonNull Log log, @NonNull String queueName);
 
     /**
      * Check whether channel is enabled or disabled.
@@ -37,4 +39,23 @@ public interface AvalancheChannel {
      * @param groupName the group name.
      */
     void clear(String groupName);
+
+    /**
+     * Channel listener specification.
+     */
+    interface Listener {
+        /**
+         * Called when the log is delivered successfully.
+         *
+         * @param log The log that is delivered.
+         */
+        void onSuccess(Log log);
+
+        /**
+         * Called when the log is not delivered successfully.
+         *
+         * @param log The log that is not delivered.
+         */
+        void onFailure(Log log);
+    }
 }
