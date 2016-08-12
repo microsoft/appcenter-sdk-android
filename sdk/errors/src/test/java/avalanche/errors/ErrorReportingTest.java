ca8e9b4e49061e47dc50703fb0829b041ce88d17
Matthias Wenz <matwen@microsoft.com>
2016-08-12 16:52:30 -0700

    Verify queueing of stored error logs

--- sdk/errors/src/test/java/avalanche/errors/ErrorReportingTest.java
+++ sdk/errors/src/test/java/avalanche/errors/ErrorReportingTest.java
@@ -2,12 +2,15 @@ package avalanche.errors;
 
 import android.content.Context;
 import android.os.SystemClock;
+import android.support.annotation.NonNull;
 
 import junit.framework.Assert;
 
+import org.json.JSONException;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentMatcher;
 import org.mockito.Mockito;
 import org.mockito.invocation.InvocationOnMock;
 import org.mockito.stubbing.Answer;
@@ -15,34 +18,42 @@ import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
+import java.io.File;
 import java.util.Map;
 
 import avalanche.core.channel.AvalancheChannel;
+import avalanche.core.ingestion.models.Log;
+import avalanche.core.ingestion.models.LogContainer;
 import avalanche.core.ingestion.models.json.LogFactory;
+import avalanche.core.ingestion.models.json.LogSerializer;
 import avalanche.core.utils.PrefStorageConstants;
 import avalanche.core.utils.StorageHelper;
 import avalanche.errors.ingestion.models.JavaErrorLog;
 import avalanche.errors.ingestion.models.json.JavaErrorLogFactory;
 import avalanche.errors.model.TestCrashException;
+import avalanche.errors.utils.ErrorLogHelper;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Matchers.anyBoolean;
+import static org.mockito.Matchers.argThat;
 import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 import static org.powermock.api.mockito.PowerMockito.mockStatic;
 import static org.powermock.api.mockito.PowerMockito.when;
 
 @RunWith(PowerMockRunner.class)
-@PrepareForTest({SystemClock.class, StorageHelper.PreferencesStorage.class})
+@PrepareForTest({ErrorLogHelper.class, SystemClock.class, StorageHelper.InternalStorage.class, StorageHelper.PreferencesStorage.class})
 public class ErrorReportingTest {
 
     @Before
     public void setUp() {
         ErrorReporting.unsetInstance();
         mockStatic(SystemClock.class);
+        mockStatic(StorageHelper.InternalStorage.class);
         mockStatic(StorageHelper.PreferencesStorage.class);
 
         when(SystemClock.elapsedRealtime()).thenReturn(System.currentTimeMillis());
@@ -86,7 +97,7 @@ public class ErrorReportingTest {
 
         assertTrue(ErrorReporting.isEnabled());
         assertTrue(ErrorReporting.getInstance().getInitializeTimestamp() > 0);
-        assertTrue(Thread.getDefaultUncaughtExceptionHandler() instanceof  UncaughtExceptionHandler);
+        assertTrue(Thread.getDefaultUncaughtExceptionHandler() instanceof UncaughtExceptionHandler);
         ErrorReporting.setEnabled(false);
         assertFalse(ErrorReporting.isEnabled());
         assertEquals(ErrorReporting.getInstance().getInitializeTimestamp(), -1);
@@ -95,6 +106,54 @@ public class ErrorReportingTest {
         assertTrue(ErrorReporting.isEnabled());
         assertTrue(ErrorReporting.getInstance().getInitializeTimestamp() > 0);
         assertTrue(Thread.getDefaultUncaughtExceptionHandler() instanceof UncaughtExceptionHandler);
+    }
+
+    @Test
+    public void queuePendingCrashes() {
+        Context mockContext = mock(Context.class);
+        AvalancheChannel mockChannel = mock(AvalancheChannel.class);
+
+        final JavaErrorLog errorLog = ErrorLogHelper.createErrorLog(mockContext, Thread.currentThread(), new RuntimeException(), Thread.getAllStackTraces(), 0);
+
+        mockStatic(ErrorLogHelper.class);
+        when(ErrorLogHelper.getStoredErrorLogFiles()).thenReturn(new File[]{new File(".")});
+
+        ErrorReporting errorReporting = ErrorReporting.getInstance();
+        errorReporting.setLogSerializer(new LogSerializer() {
+            @Override
+            public String serializeLog(@NonNull Log log) throws JSONException {
+                return null;
+            }
+
+            @Override
+            public Log deserializeLog(@NonNull String json) throws JSONException {
+                return errorLog;
+            }
+
+            @Override
+            public String serializeContainer(@NonNull LogContainer container) throws JSONException {
+                return null;
+            }
+
+            @Override
+            public LogContainer deserializeContainer(@NonNull String json) throws JSONException {
+                return null;
+            }
+
+            @Override
+            public void addLogFactory(@NonNull String logType, @NonNull LogFactory logFactory) {
+
+            }
+        });
+
+        errorReporting.onChannelReady(mockContext, mockChannel);
+
+        verify(mockChannel).enqueue(argThat(new ArgumentMatcher<Log>() {
+            @Override
+            public boolean matches(Object log) {
+                return log.equals(errorLog);
+            }
+        }), eq(errorReporting.getGroupName()));
     }
 
     @Test(expected = TestCrashException.class)
