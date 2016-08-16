e77c63de123c6240d4e1744c64c765c4fb37ffb3
Jae Lim <jaelim@microsoft.com>
2016-08-15 17:17:19 -0700

    Test application for listeners.

--- apps/sasquatch/src/main/java/avalanche/sasquatch/activities/MainActivity.java
+++ apps/sasquatch/src/main/java/avalanche/sasquatch/activities/MainActivity.java
@@ -1,17 +1,22 @@
 package avalanche.sasquatch.activities;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import avalanche.analytics.Analytics;
 import avalanche.core.Avalanche;
 import avalanche.core.utils.UUIDUtils;
+import avalanche.errors.AbstractErrorReportingListener;
 import avalanche.errors.ErrorReporting;
+import avalanche.errors.model.ErrorReport;
 import avalanche.sasquatch.R;
 import avalanche.sasquatch.features.TestFeatures;
 import avalanche.sasquatch.features.TestFeaturesListAdapter;
@@ -22,6 +27,47 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ErrorReporting.setListener(new AbstractErrorReportingListener() {
+            @Override
+            public boolean shouldAwaitUserConfirmation() {
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder
+                        .setTitle(R.string.crash_confirmation_dialog_title)
+                        .setMessage(R.string.crash_confirmation_dialog_message)
+                        .setPositiveButton(R.string.crash_confirmation_dialog_send_button, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                ErrorReporting.notifyUserConfirmation(ErrorReporting.SEND);
+                            }
+                        })
+                        .setNegativeButton(R.string.crash_confirmation_dialog_not_send_button, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                ErrorReporting.notifyUserConfirmation(ErrorReporting.DONT_SEND);
+                            }
+                        })
+                        .setNeutralButton(R.string.crash_confirmation_dialog_always_send_button, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                ErrorReporting.notifyUserConfirmation(ErrorReporting.ALWAYS_SEND);
+                            }
+                        });
+                builder.create().show();
+                return true;
+            }
+
+            @Override
+            public void onSendingFailed(ErrorReport errorReport, Exception e) {
+                Toast.makeText(MainActivity.this, R.string.crash_sent_failed, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onSendingSucceeded(ErrorReport errorReport) {
+                String message = String.format("%s\nCrash ID: %s\nThrowable: %s", R.string.crash_sent_succeeded, errorReport.getId(), errorReport.getThrowable().toString());
+                Toast.makeText(MainActivity.this, message, Toast.LENGTH_LONG).show();
+            }
+        });
 
         Avalanche.setLogLevel(Log.VERBOSE);
         Avalanche.start(getApplication(), UUIDUtils.randomUUID().toString(), Analytics.class, ErrorReporting.class);
