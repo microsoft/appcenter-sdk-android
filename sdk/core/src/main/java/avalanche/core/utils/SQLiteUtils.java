849e61fb09dba2327fe7f19f9ed476a5bc95ae36
Guillaume Perrot <guperrot@microsoft.com>
2016-08-17 19:32:15 -0700

    More testing for database

---
+++ sdk/core/src/main/java/avalanche/core/utils/SQLiteUtils.java
@@ -0,0 +1,12 @@
+package avalanche.core.utils;
+
+import android.database.sqlite.SQLiteQueryBuilder;
+import android.support.annotation.NonNull;
+
+class SQLiteUtils {
+
+    @NonNull
+    static SQLiteQueryBuilder newSQLiteQueryBuilder() {
+        return new SQLiteQueryBuilder();
+    }
+}
