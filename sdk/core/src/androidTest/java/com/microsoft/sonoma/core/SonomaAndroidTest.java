6174efdd65b769851fe121fb059f53963b75909d
Guillaume Perrot <guperrot@microsoft.com>
2016-09-20 14:07:00 -0700

    Set default log level based on debug vs release

--- sdk/core/src/androidTest/java/com/microsoft/sonoma/core/SonomaAndroidTest.java
+++ sdk/core/src/androidTest/java/com/microsoft/sonoma/core/SonomaAndroidTest.java
@@ -3,11 +3,14 @@ package com.microsoft.sonoma.core;
 import android.app.Application;
 import android.app.Instrumentation;
 import android.support.test.InstrumentationRegistry;
+import android.util.Log;
 
 import com.microsoft.sonoma.core.utils.PrefStorageConstants;
+import com.microsoft.sonoma.core.utils.SonomaLog;
 import com.microsoft.sonoma.core.utils.StorageHelper;
 import com.microsoft.sonoma.core.utils.UUIDUtils;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.UUID;
@@ -19,10 +22,18 @@ import static org.junit.Assert.assertNotNull;
 @SuppressWarnings("unused")
 public class SonomaAndroidTest {
 
+    private Application mApplication;
+
+    @Before
+    public void setUp() throws IllegalAccessException, ClassNotFoundException, InstantiationException {
+        Sonoma.unsetInstance();
+        Constants.APPLICATION_DEBUGGABLE = false;
+        mApplication = Instrumentation.newApplication(Application.class, InstrumentationRegistry.getTargetContext());
+    }
+
     @Test
     public void getInstallId() throws IllegalAccessException, ClassNotFoundException, InstantiationException {
-        Application application = Instrumentation.newApplication(Application.class, InstrumentationRegistry.getTargetContext());
-        Sonoma.start(application, UUIDUtils.randomUUID().toString(), new SonomaFeature[]{});
+        Sonoma.start(mApplication, UUIDUtils.randomUUID().toString(), new SonomaFeature[]{});
         StorageHelper.PreferencesStorage.remove(PrefStorageConstants.KEY_INSTALL_ID);
         UUID installId = Sonoma.getInstallId();
         assertNotNull(installId);
@@ -31,5 +42,12 @@ public class SonomaAndroidTest {
         UUID installId2 = Sonoma.getInstallId();
         assertNotNull(installId2);
         assertNotEquals(installId2, installId);
+    }
+
+    @Test
+    public void setDefaultLogLevelDebug() throws IllegalAccessException, ClassNotFoundException, InstantiationException {
+        SonomaLog.setLogLevel(Log.ASSERT);
+        Sonoma.start(mApplication, UUIDUtils.randomUUID().toString(), new SonomaFeature[]{});
+        assertEquals(Log.WARN, Sonoma.getLogLevel());
     }
 }
