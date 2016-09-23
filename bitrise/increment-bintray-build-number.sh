cc16b1ef6999f21525659af49847dc314dd87ffd
Guillaume Perrot <guperrot@microsoft.com>
2016-09-22 19:01:59 -0700

    Use semver.org version compatible format
    * public release remains 0.1.0
    * pre-release with git tag/release: 0.1.0-42
    * push to master:   0.1.0-43+abcdef  (no git tag, git hash metadata in version)

--- bitrise/increment-bintray-build-number.sh
+++ bitrise/increment-bintray-build-number.sh
@@ -7,9 +7,9 @@ gradleVersion=`egrep "versionName = '(.*)'" *.gradle | sed -r "s/^.*versionName 
 bintrayRelease=`curl -s https://api.bintray.com/packages/microsoftsonoma/sonoma/sonoma-core --user $BINTRAY_USER:$BINTRAY_KEY`
 bintrayVersion=`sed -r 's/^.*"latest_version":"([^"]+)",.*$/\1/' <<< "$bintrayRelease"`
 
-bintrayBaseVersion=`sed -r 's/^(([^\.]+\.){2}[^\.]+).*$/\1/' <<< "$bintrayVersion"`
+bintrayBaseVersion=`sed -r 's/^(([0-9]+\.){2}[0-9]+).*$/\1/' <<< "$bintrayVersion"`
 
-bintrayBuildNumber=`sed -r 's/^([^\.]+\.){3}(.*)$/\2/' <<< "$bintrayVersion"`
+bintrayBuildNumber=`sed -r 's/^([0-9]+\.){2}[0-9]+-([0-9]+).*$/\2/' <<< "$bintrayVersion"`
 
 if [[ "$gradleVersion" != "$bintrayBaseVersion" ]] || [[ "$bintrayVersion" == "$bintrayBuildNumber" ]]
 then
@@ -17,10 +17,15 @@ then
 else
   buildNumber=$((bintrayBuildNumber+1))
 fi
-newVersion=$gradleVersion.$buildNumber
+newVersion=$gradleVersion-$buildNumber
+
+if [[ "$1" == "--use-hash" ]]
+then
+  newVersion+="+$(git rev-parse --short HEAD)"
+fi
 
 # Replace version in the file
-sed -i -r "s#(versionName = ')(.*)'#\1$newVersion'#" *.gradle
+sed -i -b -r "s#(versionName = ')(.*)'#\1$newVersion'#" *.gradle
 
 # Print version on build logs
 grep "versionName = '" *.gradle
