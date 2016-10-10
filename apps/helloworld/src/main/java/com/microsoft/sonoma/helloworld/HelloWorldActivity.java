850563da1ab78dee372d6e30a1afe32d154ffb41
Guillaume Perrot <guperrot@microsoft.com>
2016-10-10 14:17:01 -0700

    Hello world app secret update

--- apps/helloworld/src/main/java/com/microsoft/sonoma/helloworld/HelloWorldActivity.java
+++ apps/helloworld/src/main/java/com/microsoft/sonoma/helloworld/HelloWorldActivity.java
@@ -6,7 +6,6 @@ import android.util.Log;
 
 import com.microsoft.sonoma.analytics.Analytics;
 import com.microsoft.sonoma.core.Sonoma;
-import com.microsoft.sonoma.core.utils.UUIDUtils;
 import com.microsoft.sonoma.crashes.Crashes;
 
 public class HelloWorldActivity extends Activity {
@@ -17,7 +16,7 @@ public class HelloWorldActivity extends Activity {
         setContentView(R.layout.activity_main);
 
         Sonoma.setLogLevel(Log.VERBOSE);
-        Sonoma.start(getApplication(), UUIDUtils.randomUUID().toString(), Analytics.class, Crashes.class);
+        Sonoma.start(getApplication(), "45d1d9f6-2492-4e68-bd44-7190351eb5f3", Analytics.class, Crashes.class);
     }
 
     @SuppressWarnings({"ConstantConditions", "ConstantIfStatement"})
