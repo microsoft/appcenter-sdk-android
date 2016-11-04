e2daa724f0ab84568dcb5b17aa40e81c005083b2
Jae Lim <jaelim@microsoft.com>
2016-11-04 10:00:40 -0700

    Changing package prefix for bintray

--- bitrise/put-bintray-secrets-gradle.sh
+++ bitrise/put-bintray-secrets-gradle.sh
@@ -4,4 +4,4 @@ echo "bintray.user=$BINTRAY_USER" > local.properties
 echo "bintray.key=$BINTRAY_KEY" >> local.properties
 echo "bintray.user.org=$BINTRAY_USER_ORG" >> local.properties
 echo "bintray.repo=$BINTRAY_REPO" >> local.properties
-echo "bintray.package.prefix=sonoma-" >> local.properties
+echo "bintray.package.prefix=mobilecenter" >> local.properties
