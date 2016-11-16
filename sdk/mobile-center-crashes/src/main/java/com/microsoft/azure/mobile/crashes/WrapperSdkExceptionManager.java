274e8b3c376630df15d631c6de2c5de502b5ff74
Alexander Chocron <alchocro@microsoft.com>
2016-11-15 16:00:22 -0800

    implement functionality for wrapper sdk to use callbacks

--- sdk/mobile-center-crashes/src/main/java/com/microsoft/azure/mobile/crashes/WrapperSdkExceptionManager.java
+++ sdk/mobile-center-crashes/src/main/java/com/microsoft/azure/mobile/crashes/WrapperSdkExceptionManager.java
@@ -11,43 +11,94 @@ import java.util.HashMap;
 import java.io.IOException;
 
 public class WrapperSdkExceptionManager {
+    /**
+     * File extension for data files created by this class.
+     */
+    private static final String dataFileExtension = ".dat";
+
+    /**
+     * In-memory storage to be set by a wrapper SDK that contains the data it needs to save
+     */
     private static byte[] mManagedExceptionData = null;
+
+    /**
+     * Contains wrapper SDK data that has been loaded into memory
+     */
     private static Map<String, byte[]> mManagedExceptionDataContainer = new HashMap<>();
 
+    /**
+     * Check if there is managed exception data in memory
+     *
+     * @return true if there is exception data in memory
+     */
     public static boolean hasManagedExceptionData() {
         return mManagedExceptionData != null;
     }
 
+    /**
+     * Get wrapper-specific data associated with a particular crash
+     *
+     * @param reportIdString    String representation of the associated error UUID
+     * @return byte array containing the exception data
+     */
     public static byte[] getManagedExceptionData(String reportIdString) {
         return loadManagedExceptionData(reportIdString);
     }
 
+    /**
+     * Store wrapper exception data in memory. This should only be used by a wrapper SDK.
+     *
+     * @param data  byte array containing the data a wrapper SDK needs to save
+     */
     public static void setManagedExceptionData(byte[] data) {
         mManagedExceptionData = data;
     }
 
+    /**
+     * Store the in-memory managed exception data to disk
+     *
+     * @param reportId  The associated error UUID
+     */
     public static void saveManagedExceptionData(UUID reportId) {
         mManagedExceptionDataContainer.put(reportId.toString(), mManagedExceptionData);
         File dataFile = getFile(reportId);
         try {
             StorageHelper.InternalStorage.writeObject(dataFile, mManagedExceptionData);
+            mManagedExceptionData = null;
         }
         catch (IOException e) {
-            MobileCenterLog.error(Crashes.LOG_TAG, "Error writing wrapper exception data to file", e);
+            MobileCenterLog.error(Crashes.LOG_TAG, "Error writing managed exception data to file", e);
         }
+        mManagedExceptionData = null;
     }
 
+    /**
+     * Delete managed exception data from disk and store it in memory
+     *
+     * @param reportId    The associated error UUID
+     */
     public static void deleteManagedExceptionData(UUID reportId) {
         File dataFile = getFile(reportId);
         loadManagedExceptionData(reportId); //save the exception data in memory before deleting
         StorageHelper.InternalStorage.delete(dataFile);
     }
 
-
+    /**
+     * Load managed exception data into memory
+     *
+     * @param reportId    The associated error UUID
+     * @return The data loaded into memory
+     */
     private static byte[] loadManagedExceptionData(UUID reportId) {
         return loadManagedExceptionData(reportId.toString());
     }
 
+    /**
+     * Load managed exception data into memory
+     *
+     * @param reportIdString   String representation of the associated error UUID
+     * @return The data loaded into memory
+     */
     private static byte[] loadManagedExceptionData(String reportIdString) {
         File dataFile = getFile(reportIdString);
         try {
@@ -58,20 +109,33 @@ public class WrapperSdkExceptionManager {
             return dataBytes;
         }
         catch (ClassNotFoundException ignored) {
-            MobileCenterLog.error(Crashes.LOG_TAG, "Cannot read throwable file " + dataFile.getName(), ignored);
+            MobileCenterLog.error(Crashes.LOG_TAG, "Cannot read managed exception file " + dataFile.getName(), ignored);
         }
         catch (IOException ignored) {
-            MobileCenterLog.error(Crashes.LOG_TAG, "Cannot access serialized throwable file " + dataFile.getName(), ignored);
+            MobileCenterLog.error(Crashes.LOG_TAG, "Cannot access serialized managed exception file " + dataFile.getName(), ignored);
         }
-
         return null;
     }
+
+    /**
+     * Get a file object for managed exception data
+     *
+     * @param reportId    The associated error UUID
+     * @return The corresponding file object
+     */
     private static File getFile(UUID reportId) {
         return getFile(reportId.toString());
     }
+
+    /**
+     * Get a file object for managed exception data
+     *
+     * @param reportIdString   String representation of the associated error UUID
+     * @return The corresponding file object
+     */
     private static File getFile(String reportIdString) {
         File errorStorageDirectory = ErrorLogHelper.getErrorStorageDirectory();
-        String filename = reportIdString + ".dat";
+        String filename = reportIdString + dataFileExtension;
         return new File(errorStorageDirectory, filename);
     }
 }
