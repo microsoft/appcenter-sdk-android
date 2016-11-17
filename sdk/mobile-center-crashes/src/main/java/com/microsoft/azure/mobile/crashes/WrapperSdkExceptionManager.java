adddcf46b24a0b0e8dde532b840205f027af6358
Alexander Chocron <alchocro@microsoft.com>
2016-11-17 15:19:13 -0800

    Modify wrapper exception manager and add unit tests

--- sdk/mobile-center-crashes/src/main/java/com/microsoft/azure/mobile/crashes/WrapperSdkExceptionManager.java
+++ sdk/mobile-center-crashes/src/main/java/com/microsoft/azure/mobile/crashes/WrapperSdkExceptionManager.java
@@ -1,8 +1,11 @@
 package com.microsoft.azure.mobile.crashes;
 
+import com.microsoft.azure.mobile.crashes.ingestion.models.ManagedErrorLog;
 import com.microsoft.azure.mobile.crashes.utils.ErrorLogHelper;
 import com.microsoft.azure.mobile.utils.MobileCenterLog;
 import com.microsoft.azure.mobile.utils.storage.StorageHelper;
+
+import org.json.JSONException;
 
 import java.io.File;
 import java.util.UUID;
@@ -10,99 +13,73 @@ import java.util.Map;
 import java.util.HashMap;
 import java.io.IOException;
 
+
 public class WrapperSdkExceptionManager {
     /**
      * File extension for data files created by this class.
      */
-    private static final String dataFileExtension = ".dat";
-
-    /**
-     * In-memory storage to be set by a wrapper SDK that contains the data it needs to save
-     */
-    private static byte[] mManagedExceptionData = null;
+    private static final String DATA_FILE_EXTENSION = ".dat";
 
     /**
      * Contains wrapper SDK data that has been loaded into memory
      */
-    private static Map<String, byte[]> mManagedExceptionDataContainer = new HashMap<>();
+    private static Map<String, byte[]> sWrapperExceptionDataContainer = new HashMap<>();
 
     /**
-     * Check if there is managed exception data in memory
+     * Store the in-memory wrapper exception data to disk. This should only be used by a wrapper SDK.
      *
-     * @return true if there is exception data in memory
+     * @param data  The data to be saved
+     * @param reportIdString  The associated error UUID string
      */
-    public static boolean hasManagedExceptionData() {
-        return mManagedExceptionData != null;
-    }
-
-    /**
-     * Store wrapper exception data in memory. This should only be used by a wrapper SDK.
-     *
-     * @param data  byte array containing the data a wrapper SDK needs to save
-     */
-    public static void setManagedExceptionData(byte[] data) {
-        mManagedExceptionData = data;
-    }
-
-    /**
-     * Store the in-memory managed exception data to disk
-     *
-     * @param reportId  The associated error UUID
-     */
-    public static void saveManagedExceptionData(UUID reportId) {
-        if (reportId == null) {
-            MobileCenterLog.error(Crashes.LOG_TAG, "Trying to save managed exception data with null UUID");
+    public static void saveWrapperExceptionData(byte[] data, String reportIdString) {
+        if (reportIdString == null) {
+            MobileCenterLog.error(Crashes.LOG_TAG, "Trying to save wrapper exception data with null UUID");
             return;
         }
-        mManagedExceptionDataContainer.put(reportId.toString(), mManagedExceptionData);
-        File dataFile = getFile(reportId);
+
+        sWrapperExceptionDataContainer.put(reportIdString.toString(), data);
+        File dataFile = getFile(reportIdString.toString());
         try {
-            StorageHelper.InternalStorage.writeObject(dataFile, mManagedExceptionData);
-            mManagedExceptionData = null;
+            StorageHelper.InternalStorage.writeObject(dataFile, data);
         }
         catch (IOException e) {
-            MobileCenterLog.error(Crashes.LOG_TAG, "Error writing managed exception data to file", e);
+            MobileCenterLog.error(Crashes.LOG_TAG, "Error writing wrapper exception data to file", e);
         }
-        mManagedExceptionData = null;
     }
 
     /**
-     * Delete managed exception data from disk and store it in memory
+     * Delete wrapper exception data from disk and store it in memory
      *
      * @param reportId    The associated error UUID
      */
-    public static void deleteManagedExceptionData(UUID reportId) {
+    public static void deleteWrapperExceptionData(UUID reportId) {
         if (reportId == null) {
-            MobileCenterLog.error(Crashes.LOG_TAG, "Trying to delete managed exception data with null UUID");
+            MobileCenterLog.error(Crashes.LOG_TAG, "Trying to delete wrapper exception data with null UUID");
             return;
         }
-        File dataFile = getFile(reportId);
-        loadManagedExceptionData(reportId); //save the exception data in memory before deleting
+
+        File dataFile = getFile(reportId.toString());
+        byte[] loadResult = loadWrapperExceptionData(reportId.toString());
+        if (loadResult == null) {
+            MobileCenterLog.error(Crashes.LOG_TAG, "Trying to delete wrapper exception data with null UUID");
+            return;
+        }
         StorageHelper.InternalStorage.delete(dataFile);
     }
 
     /**
-     * Load managed exception data into memory
-     *
-     * @param reportId    The associated error UUID
-     * @return The data loaded into memory
-     */
-    private static byte[] loadManagedExceptionData(UUID reportId) {
-        if (reportId == null) {
-            MobileCenterLog.error(Crashes.LOG_TAG, "Trying to load managed exception data with null UUID");
-            return null;
-        }
-        return loadManagedExceptionData(reportId.toString());
-    }
-
-    /**
-     * Load managed exception data into memory
+     * Load wrapper exception data into memory
      *
      * @param reportIdString   String representation of the associated error UUID
      * @return The data loaded into memory
      */
-    public static byte[] loadManagedExceptionData(String reportIdString) {
-        byte[] dataBytes = mManagedExceptionDataContainer.get(reportIdString);
+    public static byte[] loadWrapperExceptionData(String reportIdString) {
+        if (reportIdString == null) {
+            MobileCenterLog.error(Crashes.LOG_TAG, "Trying to load wrapper exception data with null UUID");
+            return null;
+        }
+
+        byte[] dataBytes = sWrapperExceptionDataContainer.get(reportIdString);
         if (dataBytes != null) {
             return dataBytes;
         }
@@ -112,45 +89,45 @@ public class WrapperSdkExceptionManager {
         try {
             dataBytes = StorageHelper.InternalStorage.readObject(dataFile);
             if (dataBytes != null) {
-                mManagedExceptionDataContainer.put(reportIdString, dataBytes);
+                sWrapperExceptionDataContainer.put(reportIdString, dataBytes);
             }
             return dataBytes;
         }
         catch (ClassNotFoundException e) {
-            MobileCenterLog.error(Crashes.LOG_TAG, "Cannot read managed exception data file " + dataFile.getName(), e);
+            MobileCenterLog.error(Crashes.LOG_TAG, "Cannot read wrapper exception data file " + dataFile.getName(), e);
         }
         catch (IOException e) {
-            MobileCenterLog.error(Crashes.LOG_TAG, "Cannot access managed exception data file " + dataFile.getName(), e);
+            MobileCenterLog.error(Crashes.LOG_TAG, "Cannot access wrapper exception data file " + dataFile.getName(), e);
         }
         return null;
     }
 
     /**
-     * Get a file object for managed exception data
-     *
-     * @param reportId    The associated error UUID
-     * @return The corresponding file object
-     */
-    private static File getFile(UUID reportId) {
-        if (reportId == null) {
-            return null;
-        }
-        return getFile(reportId.toString());
-    }
-
-    /**
-     * Get a file object for managed exception data
+     * Get a file object for wrapper exception data
      *
      * @param reportIdString   String representation of the associated error UUID
      * @return The corresponding file object
      */
     private static File getFile(String reportIdString) {
-        if (reportIdString == null) {
-            return null;
-        }
         File errorStorageDirectory = ErrorLogHelper.getErrorStorageDirectory();
-        String filename = reportIdString + dataFileExtension;
+        String filename = reportIdString + DATA_FILE_EXTENSION;
         return new File(errorStorageDirectory, filename);
+    }
+
+    /**
+     * Save error log modified by a wrapper SDK.
+     *
+     * @param errorLog error log to  save or overwrite.
+     */
+    @SuppressWarnings("WeakerAccess")
+    public static void saveWrapperSdkErrorLog(ManagedErrorLog errorLog) {
+        try {
+            Crashes.getInstance().saveErrorLog(errorLog, ErrorLogHelper.getErrorStorageDirectory(), errorLog.getId().toString());
+        } catch (JSONException e) {
+            MobileCenterLog.error(Crashes.LOG_TAG, "Error serializing error log to JSON", e);
+        } catch (IOException e) {
+            MobileCenterLog.error(Crashes.LOG_TAG, "Error writing error log to file", e);
+        }
     }
 }
 
