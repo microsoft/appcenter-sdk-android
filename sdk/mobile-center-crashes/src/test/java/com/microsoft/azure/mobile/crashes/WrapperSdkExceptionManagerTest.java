c352807a0f81c14ead75c1c132c19df92482a215
Alexander Chocron <alchocro@microsoft.com>
2016-11-17 15:35:30 -0800

    Add mockStatic to unit test for exception manager

--- sdk/mobile-center-crashes/src/test/java/com/microsoft/azure/mobile/crashes/WrapperSdkExceptionManagerTest.java
+++ sdk/mobile-center-crashes/src/test/java/com/microsoft/azure/mobile/crashes/WrapperSdkExceptionManagerTest.java
@@ -1,10 +1,13 @@
 package com.microsoft.azure.mobile.crashes;
 
+import com.microsoft.azure.mobile.utils.MobileCenterLog;
+import com.microsoft.azure.mobile.utils.UUIDUtils;
 import com.microsoft.azure.mobile.utils.storage.StorageHelper;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
@@ -13,11 +16,13 @@ import java.io.IOException;
 import java.util.UUID;
 
 import static org.mockito.Matchers.any;
-import static org.mockito.Mockito.when;
-import static org.powermock.api.mockito.PowerMockito.doThrow;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Matchers.eq;
 import static org.powermock.api.mockito.PowerMockito.mockStatic;
+import static org.powermock.api.mockito.PowerMockito.verifyStatic;
+
 @RunWith(PowerMockRunner.class)
-@PrepareForTest({StorageHelper.InternalStorage.class, Crashes.class})
+@PrepareForTest({MobileCenterLog.class, StorageHelper.InternalStorage.class, Crashes.class})
 
 public class WrapperSdkExceptionManagerTest {
     @Before
@@ -26,16 +31,25 @@ public class WrapperSdkExceptionManagerTest {
     }
 
     @Test
-    public void fileIOExceptions() throws IOException, ClassNotFoundException, Exception {
+    public void loadWrapperExceptionData() throws Exception {
+        mockStatic(StorageHelper.InternalStorage.class);
 
-        when(StorageHelper.InternalStorage.readObject(any(File.class))).thenThrow(new IOException()).thenThrow(new ClassNotFoundException());
-
-
+        PowerMockito.doThrow(new IOException()).when(StorageHelper.InternalStorage.class);
+        StorageHelper.InternalStorage.readObject(any(File.class));
         WrapperSdkExceptionManager.loadWrapperExceptionData(UUID.randomUUID().toString());
+        PowerMockito.doThrow(new ClassNotFoundException()).when(StorageHelper.InternalStorage.class);
+        StorageHelper.InternalStorage.readObject(any(File.class));
         WrapperSdkExceptionManager.loadWrapperExceptionData(UUID.randomUUID().toString());
+    }
 
-        doThrow(new IOException()).when(StorageHelper.InternalStorage.class);
-        StorageHelper.InternalStorage.writeObject(any(File.class), any(String.class));
-        WrapperSdkExceptionManager.saveWrapperExceptionData(new byte[]{}, UUID.randomUUID().toString());
+    @Test
+    public void saveWrapperExceptionData() throws IOException {
+        mockStatic(MobileCenterLog.class);
+        mockStatic(StorageHelper.InternalStorage.class);
+        PowerMockito.doThrow(new IOException()).when(StorageHelper.InternalStorage.class);
+        StorageHelper.InternalStorage.writeObject(any(File.class), anyString());
+        WrapperSdkExceptionManager.saveWrapperExceptionData(null, UUIDUtils.randomUUID().toString());
+        verifyStatic();
+        MobileCenterLog.error(eq(Crashes.LOG_TAG), anyString(), any(IOException.class));
     }
 }
