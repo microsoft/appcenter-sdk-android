f9dd989c1a7454f856854b263ceb44e25dc6e5dd
Alexander Chocron <alchocro@microsoft.com>
2016-11-18 14:03:32 -0800

    Make test cases verify results and cleaner

--- sdk/mobile-center-crashes/src/test/java/com/microsoft/azure/mobile/crashes/WrapperSdkExceptionManagerTest.java
+++ sdk/mobile-center-crashes/src/test/java/com/microsoft/azure/mobile/crashes/WrapperSdkExceptionManagerTest.java
@@ -7,14 +7,17 @@ import com.microsoft.azure.mobile.utils.storage.StorageHelper;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.Serializable;
 import java.util.UUID;
 
+import static junit.framework.Assert.assertNull;
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Matchers.eq;
@@ -39,29 +42,40 @@ public class WrapperSdkExceptionManagerTest {
     public void loadWrapperExceptionData() throws Exception {
         PowerMockito.doThrow(new IOException()).when(StorageHelper.InternalStorage.class);
         StorageHelper.InternalStorage.readObject(any(File.class));
-        WrapperSdkExceptionManager.loadWrapperExceptionData(UUID.randomUUID());
+        assertNull(WrapperSdkExceptionManager.loadWrapperExceptionData(UUID.randomUUID()));
         PowerMockito.doThrow(new ClassNotFoundException()).when(StorageHelper.InternalStorage.class);
         StorageHelper.InternalStorage.readObject(any(File.class));
-        WrapperSdkExceptionManager.loadWrapperExceptionData(UUID.randomUUID());
-
-        WrapperSdkExceptionManager.loadWrapperExceptionData(null);
-
+        assertNull(WrapperSdkExceptionManager.loadWrapperExceptionData(UUID.randomUUID()));
+        assertNull(WrapperSdkExceptionManager.loadWrapperExceptionData(null));
     }
 
     @Test
     public void deleteWrapperExceptionDataWithNullId() throws Exception {
         WrapperSdkExceptionManager.deleteWrapperExceptionData(null);
+        verifyStatic(Mockito.never());
+        StorageHelper.InternalStorage.delete(any(File.class));
+
+        WrapperSdkExceptionManager.deleteWrapperExceptionData(UUID.randomUUID());
+        verifyStatic(Mockito.never());
+        StorageHelper.InternalStorage.delete(any(File.class));
     }
 
     @Test
     public void saveWrapperExceptionData() throws IOException {
+        // cannot save with a null uuid
+        WrapperSdkExceptionManager.saveWrapperExceptionData(null, null);
+        verifyStatic(Mockito.never());
+        StorageHelper.InternalStorage.writeObject(any(File.class), anyString());
+
+        // ok to save null if there is a uuid
+        WrapperSdkExceptionManager.saveWrapperExceptionData(null, UUID.randomUUID()); //save null data
+        verifyStatic();
+        StorageHelper.InternalStorage.writeObject(any(File.class), any(Serializable.class));
+        
         PowerMockito.doThrow(new IOException()).when(StorageHelper.InternalStorage.class);
         StorageHelper.InternalStorage.writeObject(any(File.class), anyString());
         WrapperSdkExceptionManager.saveWrapperExceptionData(null, UUIDUtils.randomUUID());
         verifyStatic();
         MobileCenterLog.error(eq(Crashes.LOG_TAG), anyString(), any(IOException.class));
-
-        WrapperSdkExceptionManager.saveWrapperExceptionData(null, null);
-
     }
 }
