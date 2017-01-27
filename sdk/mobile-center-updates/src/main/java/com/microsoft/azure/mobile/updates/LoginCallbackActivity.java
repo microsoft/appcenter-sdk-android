8b32e81622b0597342cb899050f0336d20391e68
Guillaume Perrot <guperrot@microsoft.com>
2017-01-26 17:01:25 -0800

    Store token and don't launch browser if we already stored it

--- sdk/mobile-center-updates/src/main/java/com/microsoft/azure/mobile/updates/LoginCallbackActivity.java
+++ sdk/mobile-center-updates/src/main/java/com/microsoft/azure/mobile/updates/LoginCallbackActivity.java
@@ -6,19 +6,31 @@ import android.os.Bundle;
 
 import com.microsoft.azure.mobile.utils.MobileCenterLog;
 
+import static com.microsoft.azure.mobile.updates.Updates.EXTRA_UPDATE_TOKEN;
 import static com.microsoft.azure.mobile.updates.Updates.LOG_TAG;
 
 public class LoginCallbackActivity extends Activity {
 
-    private static final String EXTRA_COOKIE = "cookie";
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
+
+        /*
+         * Get update token from intent.
+         * TODO protect intent: verifying signature with server public key in another field seems like a good way.
+         * But it would not protect against spamming intents to cause app to use CPU to verify fake signatures.
+         */
         super.onCreate(savedInstanceState);
         Intent intent = getIntent();
-        String cookie = intent.getStringExtra(EXTRA_COOKIE);
-        MobileCenterLog.debug(LOG_TAG, "LoginCallbackActivity.getIntent()=" + intent);
-        MobileCenterLog.verbose(LOG_TAG, "LoginCallbackActivity.getIntent()#S.cookie=" + cookie);
+        String updateToken = intent.getStringExtra(EXTRA_UPDATE_TOKEN);
+        MobileCenterLog.debug(LOG_TAG, getLocalClassName() + ".getIntent()=" + intent);
+        MobileCenterLog.verbose(LOG_TAG, getLocalClassName() + ".getIntent()#S.update_token=" + updateToken);
+
+        /* Store update token. */
+        if (updateToken != null) {
+            Updates.getInstance().storeUpdateToken(this, updateToken);
+        }
+
+        /* Resume app to avoid staying on browser if no application task. */
         if (isTaskRoot()) {
             startActivity(getPackageManager().getLaunchIntentForPackage(getPackageName()));
         }
