a00bf2a58537e5ccff844992dab990b5515529c3
Ivan Matkov <v-ivmatk@microsoft.com>
2017-02-20 17:26:39 +0300

    Add async task for get push token.

--- sdk/mobile-center-push/src/main/java/com/microsoft/azure/mobile/push/Push.java
+++ sdk/mobile-center-push/src/main/java/com/microsoft/azure/mobile/push/Push.java
@@ -1,15 +1,21 @@
 package com.microsoft.azure.mobile.push;
 
 import android.content.Context;
+import android.os.AsyncTask;
 import android.support.annotation.NonNull;
+import android.support.annotation.VisibleForTesting;
 
+import com.google.android.gms.gcm.GoogleCloudMessaging;
+import com.google.android.gms.iid.InstanceID;
 import com.microsoft.azure.mobile.AbstractMobileCenterService;
 import com.microsoft.azure.mobile.channel.Channel;
 import com.microsoft.azure.mobile.ingestion.models.json.LogFactory;
 import com.microsoft.azure.mobile.push.ingestion.models.PushInstallationLog;
 import com.microsoft.azure.mobile.push.ingestion.models.json.PushInstallationLogFactory;
+import com.microsoft.azure.mobile.utils.AsyncTaskUtils;
 import com.microsoft.azure.mobile.utils.MobileCenterLog;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -29,9 +35,19 @@ public class Push extends AbstractMobileCenterService {
     public static final String LOG_TAG = MobileCenterLog.LOG_TAG + SERVICE_NAME;
 
     /**
+     * Constant marking event of the push group.
+     */
+    private static final String PUSH_GROUP = "group_push";
+
+    /**
      * Log factories managed by this service.
      */
     private final Map<String, LogFactory> mFactories;
+
+    /**
+     * Application context, if not null it means onStart was called.
+     */
+    private Context mContext;
 
     /**
      * Init.
@@ -41,9 +57,20 @@ public class Push extends AbstractMobileCenterService {
         mFactories.put(PushInstallationLog.TYPE, new PushInstallationLogFactory());
     }
 
+    /**
+     * Enqueue a start session log
+     *
+     * @param pushToken
+     */
+    private void enqueuePushInstallationLog(String pushToken) {
+        PushInstallationLog log = new PushInstallationLog();
+        log.setPushToken(pushToken);
+        mChannel.enqueue(log, PUSH_GROUP);
+    }
+
     @Override
     protected String getGroupName() {
-        return null;
+        return PUSH_GROUP;
     }
 
     @Override
@@ -59,5 +86,29 @@ public class Push extends AbstractMobileCenterService {
     @Override
     public Map<String, LogFactory> getLogFactories() {
         return mFactories;
+    }
+
+    @Override
+    public synchronized void onStarted(@NonNull Context context, @NonNull String appSecret, @NonNull Channel channel) {
+        super.onStarted(context, appSecret, channel);
+        mContext = context;
+
+        // TODO AsyncTaskUtils.execute(LOG_TAG, new PushTokenTask(), senderId);
+    }
+
+    @VisibleForTesting
+    class PushTokenTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... params) {
+            String senderId = params[0];
+            InstanceID instanceID = InstanceID.getInstance(mContext);
+            try {
+                return instanceID.getToken(senderId,  GoogleCloudMessaging.INSTANCE_ID_SCOPE, null);
+            } catch (IOException e) {
+                MobileCenterLog.error(LOG_TAG, "Cannot get push token", e);
+                return null;
+            }
+        }
     }
 }
