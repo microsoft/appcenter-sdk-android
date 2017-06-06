c2c945c0e1ee49beefe1f2c9b9e070d2f3cad515
Guillaume Perrot <guperrot@microsoft.com>
2017-06-06 14:42:01 -0700

    Add missing tests for DefaultSimpleFuture

---
+++ sdk/mobile-center/src/test/java/com/microsoft/azure/mobile/utils/async/SimpleFutureTest.java
@@ -0,0 +1,97 @@
+package com.microsoft.azure.mobile.utils.async;
+
+import com.microsoft.azure.mobile.utils.HandlerUtils;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.powermock.api.mockito.PowerMockito.doAnswer;
+import static org.powermock.api.mockito.PowerMockito.mockStatic;
+
+@RunWith(PowerMockRunner.class)
+public class SimpleFutureTest {
+
+    @Test
+    public void getWithInterruption() throws InterruptedException {
+        final DefaultSimpleFuture<Boolean> future = new DefaultSimpleFuture<>();
+        final AtomicReference<Boolean> result = new AtomicReference<>();
+        Thread thread = new Thread() {
+
+            @Override
+            public void run() {
+                result.set(future.get());
+            }
+        };
+        thread.start();
+        thread.interrupt();
+        future.complete(true);
+        thread.join();
+        assertEquals(true, result.get());
+    }
+
+    @Test
+    public void isDoneWithInterruption() throws InterruptedException {
+        final DefaultSimpleFuture<Boolean> future = new DefaultSimpleFuture<>();
+        final AtomicReference<Boolean> result = new AtomicReference<>();
+        Thread thread = new Thread() {
+
+            @Override
+            public void run() {
+                //noinspection StatementWithEmptyBody
+                while (!isInterrupted()) {
+                }
+                result.set(future.isDone());
+            }
+        };
+        thread.start();
+        thread.interrupt();
+        future.complete(true);
+        thread.join();
+        assertEquals(true, result.get());
+    }
+
+    @Test
+    public void completeTwiceIgnored() throws InterruptedException {
+        DefaultSimpleFuture<Integer> future = new DefaultSimpleFuture<>();
+        future.complete(1);
+        future.complete(2);
+        assertEquals(Integer.valueOf(1), future.get());
+    }
+
+    @Test
+    @PrepareForTest(HandlerUtils.class)
+    public void multipleCallbacks() throws InterruptedException {
+        mockStatic(HandlerUtils.class);
+        doAnswer(new Answer<Void>() {
+
+            @Override
+            public Void answer(InvocationOnMock invocation) throws Throwable {
+                ((Runnable) invocation.getArguments()[0]).run();
+                return null;
+            }
+        }).when(HandlerUtils.class);
+        HandlerUtils.runOnUiThread(any(Runnable.class));
+        DefaultSimpleFuture<Integer> future = new DefaultSimpleFuture<>();
+
+        @SuppressWarnings("unchecked")
+        SimpleFunction<Integer> function = mock(SimpleFunction.class);
+        future.thenApply(function).thenApply(function);
+        future.complete(1);
+        verify(function, times(2)).apply(1);
+
+        /* Works also after completion. */
+        future.thenApply(function).thenApply(function);
+        verify(function, times(4)).apply(1);
+    }
+}
