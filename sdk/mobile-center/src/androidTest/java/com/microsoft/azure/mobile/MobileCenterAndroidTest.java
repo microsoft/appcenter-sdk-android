e7403ae07082c9c92e746ff63f727d812c82a67d
Guillaume Perrot <guperrot@microsoft.com>
2017-06-07 19:28:13 -0700

    Fix code coverage
    For some reason the crashes android code coverage does not count for core

--- sdk/mobile-center/src/androidTest/java/com/microsoft/azure/mobile/MobileCenterAndroidTest.java
+++ sdk/mobile-center/src/androidTest/java/com/microsoft/azure/mobile/MobileCenterAndroidTest.java
@@ -2,13 +2,18 @@ package com.microsoft.azure.mobile;
 
 import android.app.Application;
 import android.app.Instrumentation;
+import android.content.Context;
+import android.support.annotation.NonNull;
 import android.support.test.InstrumentationRegistry;
 import android.util.Log;
 
+import com.microsoft.azure.mobile.channel.Channel;
 import com.microsoft.azure.mobile.utils.MobileCenterLog;
 import com.microsoft.azure.mobile.utils.PrefStorageConstants;
 import com.microsoft.azure.mobile.utils.UUIDUtils;
+import com.microsoft.azure.mobile.utils.async.DefaultSimpleFuture;
 import com.microsoft.azure.mobile.utils.async.SimpleConsumer;
+import com.microsoft.azure.mobile.utils.async.SimpleFuture;
 import com.microsoft.azure.mobile.utils.storage.StorageHelper;
 
 import org.junit.After;
@@ -46,10 +51,11 @@ public class MobileCenterAndroidTest {
         assertNull(MobileCenter.getInstallId().get());
         StorageHelper.initialize(mApplication);
         StorageHelper.PreferencesStorage.remove(PrefStorageConstants.KEY_INSTALL_ID);
-        MobileCenter.start(mApplication, UUIDUtils.randomUUID().toString());
+        MobileCenter.start(mApplication, UUIDUtils.randomUUID().toString(), DummyService.class);
         UUID installId = MobileCenter.getInstallId().get();
         assertNotNull(installId);
         assertEquals(installId, MobileCenter.getInstallId().get());
+        assertEquals(installId, DummyService.getUUID().get());
         StorageHelper.PreferencesStorage.remove(PrefStorageConstants.KEY_INSTALL_ID);
         final UUID installId2 = MobileCenter.getInstallId().get();
         assertNotNull(installId2);
@@ -75,5 +81,51 @@ public class MobileCenterAndroidTest {
         MobileCenterLog.setLogLevel(Log.ASSERT);
         MobileCenter.start(mApplication, UUIDUtils.randomUUID().toString());
         assertEquals(Log.WARN, MobileCenter.getLogLevel());
+    }
+
+    private static class DummyService extends AbstractMobileCenterService {
+
+        private static DummyService sInstance = new DummyService();
+
+        private static UUID mInstallId;
+
+        static SimpleFuture<UUID> getUUID() {
+            final DefaultSimpleFuture<UUID> future = new DefaultSimpleFuture<>();
+            getInstance().post(new Runnable() {
+
+                @Override
+                public void run() {
+                    future.complete(mInstallId);
+                }
+            });
+            return future;
+        }
+
+        public static DummyService getInstance() {
+            return sInstance;
+        }
+
+        @Override
+        public String getServiceName() {
+            return "Dummy";
+        }
+
+        @Override
+        protected String getGroupName() {
+            return null;
+        }
+
+        @Override
+        protected String getLoggerTag() {
+            return null;
+        }
+
+        @Override
+        public synchronized void onStarted(@NonNull Context context, @NonNull String appSecret, @NonNull Channel channel) {
+            super.onStarted(context, appSecret, channel);
+
+            /* Check no dead lock if we do that. */
+            mInstallId = MobileCenter.getInstallId().get();
+        }
     }
 }
