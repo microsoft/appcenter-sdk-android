a6c34f95bb6972fce677f964d25cbedf74b024f0
Guillaume Perrot <guperrot@microsoft.com>
2017-09-14 12:15:45 -0700

    Add empty test classes

---
+++ sdk/mobile-center-rum/src/androidTest/java/com/microsoft/azure/mobile/rum/RealUserMeasurementsAndroidTest.java
@@ -0,0 +1,11 @@
+package com.microsoft.azure.mobile.rum;
+
+import org.junit.Test;
+
+public class RealUserMeasurementsAndroidTest {
+
+    @Test
+    public void test() {
+        // TODO
+    }
+}
