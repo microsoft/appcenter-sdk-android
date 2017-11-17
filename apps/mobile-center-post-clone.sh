435b74cd79bba3df58a21d71de2d62b2fe4b3a39
Guillaume Perrot <guperrot@microsoft.com>
2017-11-16 16:35:30 -0800

    Try to fix post clone so that it works on both int and prod

--- apps/mobile-center-post-clone.sh
+++ apps/mobile-center-post-clone.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 echo "Executing post clone script in `pwd`"
-echo $GOOGLE_SERVICES_JSON | base64 -D > sasquatch/google-services.json
+echo $GOOGLE_SERVICES_JSON | base64 -D > $APPCENTER_SOURCE_DIRECTORY/apps/sasquatch/google-services.json
