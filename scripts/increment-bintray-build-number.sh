c2b5cde33f004bf572dd27949b06e68b7173cc62
Guillaume Perrot <guperrot@microsoft.com>
2017-11-20 16:49:15 -0800

    Port increment to script to VSTS + MacOS

--- scripts/increment-bintray-build-number.sh
+++ scripts/increment-bintray-build-number.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 set -e
 
+# Get credentials
+BINTRAY_USER=${1:-$BINTRAY_USER}
+BINTRAY_KEY=${2:-$BINTRAY_KEY}
+
 # Compute new version
-gradleVersion=`egrep "versionName = '(.*)'" *.gradle | sed -r "s/^.*versionName = '(.*)'.*$/\1/"`
+gradleVersion=`egrep "versionName = '(.*)'" *.gradle | sed -E "s/^.*versionName = '(.*)'.*$/\1/"`
 
 bintrayRelease=`curl -s https://api.bintray.com/packages/${BINTRAY_USER_ORG}/${BINTRAY_REPO}/appcenter --user $BINTRAY_USER:$BINTRAY_KEY`
-bintrayVersion=`sed -r 's/^.*"latest_version":"([^"]+)",.*$/\1/' <<< "$bintrayRelease"`
+bintrayVersion=`sed -E 's/^.*"latest_version":"([^"]+)",.*$/\1/' <<< "$bintrayRelease"`
 
-bintrayBaseVersion=`sed -r 's/^(([0-9]+\.){2}[0-9]+).*$/\1/' <<< "$bintrayVersion"`
+bintrayBaseVersion=`sed -E 's/^(([0-9]+\.){2}[0-9]+).*$/\1/' <<< "$bintrayVersion"`
 
-bintrayBuildNumber=`sed -r 's/^([0-9]+\.){2}[0-9]+-([0-9]+).*$/\2/' <<< "$bintrayVersion"`
+bintrayBuildNumber=`sed -E 's/^([0-9]+\.){2}[0-9]+-([0-9]+).*$/\2/' <<< "$bintrayVersion"`
 
 if [[ "$gradleVersion" != "$bintrayBaseVersion" ]] || [[ "$bintrayVersion" == "$bintrayBuildNumber" ]]
 then
@@ -25,7 +29,7 @@ then
 fi
 
 # Replace version in the file
-sed -i -b -r "s#(versionName = ')(.*)'#\1$newVersion'#" *.gradle
+sed -E -i '' "s#(versionName = ')(.*)'#\1$newVersion'#" *.gradle
 
 # Print version on build logs
 grep "versionName = '" *.gradle
