2b42702a43031671faac32cc2f01d50a978029b0
Guillaume Perrot <guperrot@microsoft.com>
2017-12-05 20:14:58 -0800

    Fix more warnings due to imports, and print those

--- sdk/appcenter-distribute/src/main/java/com/microsoft/appcenter/distribute/Distribute.java
+++ sdk/appcenter-distribute/src/main/java/com/microsoft/appcenter/distribute/Distribute.java
@@ -9,7 +9,6 @@ import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
-import android.app.ProgressDialog;
 import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.DialogInterface;
@@ -191,7 +190,7 @@ public class Distribute extends AbstractAppCenterService {
      * They will always keep this dialog to remain compatible but just mark it deprecated.
      */
     @SuppressWarnings("deprecation")
-    private ProgressDialog mProgressDialog;
+    private android.app.ProgressDialog mProgressDialog;
 
     /**
      * Mandatory download completed in app notification.
@@ -1538,10 +1537,10 @@ public class Distribute extends AbstractAppCenterService {
      */
     @SuppressWarnings("deprecation")
     private void showDownloadProgress() {
-        mProgressDialog = new ProgressDialog(mForegroundActivity);
+        mProgressDialog = new android.app.ProgressDialog(mForegroundActivity);
         mProgressDialog.setTitle(R.string.appcenter_distribute_downloading_mandatory_update);
         mProgressDialog.setCancelable(false);
-        mProgressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+        mProgressDialog.setProgressStyle(android.app.ProgressDialog.STYLE_HORIZONTAL);
         mProgressDialog.setIndeterminate(true);
         mProgressDialog.setProgressNumberFormat(null);
         mProgressDialog.setProgressPercentFormat(null);
@@ -1554,7 +1553,7 @@ public class Distribute extends AbstractAppCenterService {
     @SuppressWarnings("deprecation")
     private synchronized void hideProgressDialog() {
         if (mProgressDialog != null) {
-            final ProgressDialog progressDialog = mProgressDialog;
+            final android.app.ProgressDialog progressDialog = mProgressDialog;
             mProgressDialog = null;
 
             /* This can be called from background check download task. */
