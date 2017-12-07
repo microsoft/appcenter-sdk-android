2b31b43610cddf92df943b1af137f967de44261b
Patrick Nikoletich <patrick.nikoletich@microsoft.com>
2017-12-07 16:17:42 +0100

    Removing extra carriage return.

--- apps/sasquatch/src/main/java/com/microsoft/appcenter/sasquatch/activities/MainActivity.java
+++ apps/sasquatch/src/main/java/com/microsoft/appcenter/sasquatch/activities/MainActivity.java
@@ -82,7 +82,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
         setContentView(R.layout.activity_main);
 
         sSharedPreferences = getSharedPreferences("Sasquatch", Context.MODE_PRIVATE);
