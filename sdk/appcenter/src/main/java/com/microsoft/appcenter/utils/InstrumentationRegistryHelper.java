ba390810c96982344a2764120a8a42d7adb1ffd1
Alexander Chocron <alchocro@microsoft.com>
2017-12-07 12:21:52 -0800

    Add javadoc

--- sdk/appcenter/src/main/java/com/microsoft/appcenter/utils/InstrumentationRegistryHelper.java
+++ sdk/appcenter/src/main/java/com/microsoft/appcenter/utils/InstrumentationRegistryHelper.java
@@ -7,6 +7,13 @@ import android.support.test.InstrumentationRegistry;
  * Wraps InstrumentationRegistry class to enable mocking in unit tests.
  */
 public class InstrumentationRegistryHelper {
+
+    /**
+     * Get the instrumentation arguments from the InstrumentationRegistry. Wrapper exists for unit
+     * tests.
+     *
+     * @return the instrumentation arguments
+     */
     public static Bundle getArguments() throws NoClassDefFoundError, IllegalAccessError {
         return InstrumentationRegistry.getArguments();
     }
