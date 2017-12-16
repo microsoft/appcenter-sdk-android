6142db02932662042f1c06bd563a391193e909cf
Guillaume Perrot <guperrot@microsoft.com>
2017-12-15 17:46:43 -0800

    Remove last reference to breakpad term in sdk part

--- sdk/appcenter-crashes/src/main/java/com/microsoft/appcenter/crashes/model/NativeException.java
+++ sdk/appcenter-crashes/src/main/java/com/microsoft/appcenter/crashes/model/NativeException.java
@@ -2,7 +2,7 @@ package com.microsoft.appcenter.crashes.model;
 
 public class NativeException extends RuntimeException {
 
-    private static final String CRASH_MESSAGE = "Native exception captured by Breakpad";
+    private static final String CRASH_MESSAGE = "Native exception read from a minidump file";
 
     public NativeException() {
         super(CRASH_MESSAGE);
