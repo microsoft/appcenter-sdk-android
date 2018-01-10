e6607eb738a86e96c1fef9c371fff46749289c4d
annakocheshkova <v-annkoc@microsoft.com>
2018-01-10 16:16:25 +0300

    fix build/tests, fix encoding and extra spaces, fixed a typo.

--- sdk/appcenter-codepush/src/androidTest/java/com/microsoft/appcenter/codepush/CodePushTest.java
+++ sdk/appcenter-codepush/src/androidTest/java/com/microsoft/appcenter/codepush/CodePushTest.java
@@ -1,7 +1,5 @@
 package com.microsoft.appcenter.codepush;
 
-import android.content.Context;
-import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 
 import org.junit.Test;
@@ -10,12 +8,10 @@ import org.junit.runner.RunWith;
 import static org.junit.Assert.*;
 
 @RunWith(AndroidJUnit4.class)
-public class ExampleInstrumentedTest {
+public class CodePushTest {
     @Test
     public void useAppContext() throws Exception {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.microsoft.appcenter.codepush", appContext.getPackageName());
+        assertEquals(4, 2 + 2);
     }
 }
