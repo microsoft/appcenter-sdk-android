a13a7697f41953d7ea2779ca0cf3cb3df51067ae
Di Hei <dihei@microsoft.com>
2018-01-09 17:35:01 -0800

    update sasquatch test for Crashes.trackException with properties

--- apps/sasquatch/src/main/java/com/microsoft/appcenter/crashes/CrashesPrivateHelper.java
+++ apps/sasquatch/src/main/java/com/microsoft/appcenter/crashes/CrashesPrivateHelper.java
@@ -1,12 +1,14 @@
 package com.microsoft.appcenter.crashes;
 
+import java.util.Map;
+
 public final class CrashesPrivateHelper {
 
     private CrashesPrivateHelper() {
     }
 
-    public static void trackException(Throwable throwable) {
-        Crashes.trackException(throwable);
+    public static void trackException(Throwable throwable, Map<String, String> properties) {
+        Crashes.trackException(throwable, properties);
     }
 
     public static void saveUncaughtException(Thread thread, Throwable exception) {
