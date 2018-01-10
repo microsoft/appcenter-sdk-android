914b59c311198491a8d3749cbbd3d34b3303eb12
annakocheshkova <v-annkoc@microsoft.com>
2018-01-10 16:05:13 +0300

    Created tests.

---
+++ sdk/appcenter-codepush/src/test/java/com/microsoft/appcenter/codepush/CodePushTest.java
@@ -0,0 +1,12 @@
+package com.microsoft.appcenter.codepush;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CodePushTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
