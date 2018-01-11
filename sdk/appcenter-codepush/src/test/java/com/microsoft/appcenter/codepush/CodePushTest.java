169f92f6374c745870e69f9a3e45925b9114b27b
annakocheshkova <v-annkoc@microsoft.com>
2018-01-11 11:49:18 +0300

    Added test for all the other enums.

--- sdk/appcenter-codepush/src/test/java/com/microsoft/appcenter/codepush/CodePushTest.java
+++ sdk/appcenter-codepush/src/test/java/com/microsoft/appcenter/codepush/CodePushTest.java
@@ -1,6 +1,10 @@
 package com.microsoft.appcenter.codepush;
 
 import com.microsoft.appcenter.codepush.enums.CodePushCheckFrequency;
+import com.microsoft.appcenter.codepush.enums.CodePushDeploymentStatus;
+import com.microsoft.appcenter.codepush.enums.CodePushInstallMode;
+import com.microsoft.appcenter.codepush.enums.CodePushSyncStatus;
+import com.microsoft.appcenter.codepush.enums.CodePushUpdateState;
 
 import org.junit.Test;
 
@@ -13,5 +17,21 @@ public class CodePushTest {
         CodePushCheckFrequency codePushCheckFrequency = CodePushCheckFrequency.MANUAL;
         int checkFrequencyValue = codePushCheckFrequency.getValue();
         assertEquals(checkFrequencyValue, 2);
+
+        CodePushDeploymentStatus codePushDeploymentStatus = CodePushDeploymentStatus.SUCCEEDED;
+        String deploymentStatusValue = codePushDeploymentStatus.getValue();
+        assertEquals(deploymentStatusValue, "DeploymentSucceeded");
+
+        CodePushInstallMode codePushInstallMode = CodePushInstallMode.IMMEDIATE;
+        int installModeValue = codePushInstallMode.getValue();
+        assertEquals(installModeValue, 0);
+
+        CodePushSyncStatus codePushSyncStatus = CodePushSyncStatus.AWAITING_USER_ACTION;
+        int syncStatusValue = codePushSyncStatus.getValue();
+        assertEquals(syncStatusValue, 6);
+
+        CodePushUpdateState codePushUpdateState = CodePushUpdateState.LATEST;
+        int updateStateValue = codePushUpdateState.getValue();
+        assertEquals(updateStateValue, 2);
     }
 }
