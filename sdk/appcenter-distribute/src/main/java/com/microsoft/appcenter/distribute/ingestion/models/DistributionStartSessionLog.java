3999725f776a2f583256c442eaf2665a1a4df044
Roland Chernov (Akvelon) <v-rolche@microsoft.com>
2018-02-21 08:26:58 +0000

    Add sending of distribution start session log after enabling in-app updates (first app launch after installation)

---
+++ sdk/appcenter-distribute/src/main/java/com/microsoft/appcenter/distribute/ingestion/models/DistributionStartSessionLog.java
@@ -0,0 +1,16 @@
+package com.microsoft.appcenter.distribute.ingestion.models;
+
+import com.microsoft.appcenter.ingestion.models.AbstractLog;
+
+/**
+ * Distribution start session log.
+ */
+public class DistributionStartSessionLog extends AbstractLog {
+
+    public static final String TYPE = "distributionStartSession";
+
+    @Override
+    public String getType() {
+        return TYPE;
+    }
+}
