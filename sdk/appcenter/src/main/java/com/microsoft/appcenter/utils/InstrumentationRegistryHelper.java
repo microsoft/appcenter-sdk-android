e87f8aca259d70b6cfc3d2376295ad0e84e3a87f
Guillaume Perrot <guperrot@microsoft.com>
2018-04-26 11:32:59 -0700

    Catch another exception when reading instrumentation

--- sdk/appcenter/src/main/java/com/microsoft/appcenter/utils/InstrumentationRegistryHelper.java
+++ sdk/appcenter/src/main/java/com/microsoft/appcenter/utils/InstrumentationRegistryHelper.java
@@ -12,9 +12,13 @@ public class InstrumentationRegistryHelper {
      * Get the instrumentation arguments from the InstrumentationRegistry. Wrapper exists for unit
      * tests.
      *
-     * @return the instrumentation arguments
+     * @return the instrumentation arguments.
+     * @throws NoClassDefFoundError     if the class is not found, typically no test dependencies in release.
+     * @throws NoSuchMethodError        if method not found, can happen with proguard.
+     * @throws IllegalAccessError       if a reflection error happens.
+     * @throws IllegalArgumentException if no argument Bundle has been registered.
      */
-    public static Bundle getArguments() throws NoClassDefFoundError, NoSuchMethodError, IllegalAccessError {
+    public static Bundle getArguments() throws NoClassDefFoundError, NoSuchMethodError, IllegalAccessError, IllegalArgumentException {
         return InstrumentationRegistry.getArguments();
     }
 }
