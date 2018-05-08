89f1367ff7ed1d76b7567531fe5bbd60f59fd39e
Ivan Matkov <v-ivmatk@microsoft.com>
2018-05-08 14:06:17 +0300

    Add missing javadocs

--- sdk/appcenter/src/main/java/com/microsoft/appcenter/channel/OneCollectorChannelListener.java
+++ sdk/appcenter/src/main/java/com/microsoft/appcenter/channel/OneCollectorChannelListener.java
@@ -9,12 +9,24 @@ import com.microsoft.appcenter.ingestion.models.Log;
  */
 public class OneCollectorChannelListener implements Channel.Listener {
 
+    /**
+     * Maximum time interval in milliseconds after which a synchronize will be triggered, regardless of queue size.
+     */
     static final int ONE_COLLECTOR_TRIGGER_INTERVAL = 3 * 1000;
 
+    /**
+     * Number of metrics queue items which will trigger synchronization.
+     */
     static final int ONE_COLLECTOR_TRIGGER_COUNT = 50;
 
+    /**
+     * Maximum number of requests being sent for the group.
+     */
     static final int ONE_COLLECTOR_TRIGGER_MAX_PARALLEL_REQUESTS = 3;
 
+    /**
+     * Postfix for One Collector's groups.
+     */
     static final String ONE_COLLECTOR_GROUP_NAME_POSTFIX = "/one";
 
     private Channel mChannel;
