03da65a3ec0b7db45be828338c9bd46cce5bf2f7
Guillaume Perrot <guperrot@microsoft.com>
2018-05-08 18:43:06 -0700

    Cleanup imports

--- sdk/appcenter-push/src/test/java/com/microsoft/appcenter/push/PushNotifierTest.java
+++ sdk/appcenter-push/src/test/java/com/microsoft/appcenter/push/PushNotifierTest.java
@@ -14,7 +14,6 @@ import android.graphics.drawable.AdaptiveIconDrawable;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
-import android.support.v4.app.NotificationCompat;
 import android.text.TextUtils;
 
 import com.microsoft.appcenter.test.TestUtils;
@@ -42,7 +41,6 @@ import static org.mockito.Matchers.anyInt;
 import static org.mockito.Matchers.anyLong;
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Matchers.eq;
-import static org.mockito.Matchers.notNull;
 import static org.mockito.Matchers.same;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
