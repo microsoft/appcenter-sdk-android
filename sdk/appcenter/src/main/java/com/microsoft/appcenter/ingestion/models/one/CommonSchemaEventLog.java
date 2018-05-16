8ffedbbe31e0594d98251ae8cadaae4edc36ee27
Guillaume Perrot <guperrot@microsoft.com>
2018-05-15 20:02:13 -0700

    WIP common schema

---
+++ sdk/appcenter/src/main/java/com/microsoft/appcenter/ingestion/models/one/CommonSchemaEventLog.java
@@ -0,0 +1,138 @@
+package com.microsoft.appcenter.ingestion.models.one;
+
+import android.support.annotation.VisibleForTesting;
+
+import com.microsoft.appcenter.ingestion.models.AbstractLog;
+import com.microsoft.appcenter.ingestion.models.json.JSONDateUtils;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONStringer;
+
+/**
+ * Common schema has 1 log type with extensions, everything is called an event.
+ * Part B can be used in the future for domain specific typing (like reflecting AppCenter log type).
+ */
+public abstract class CommonSchemaEventLog extends AbstractLog {
+
+    /**
+     * Common schema version property.
+     */
+    @VisibleForTesting
+    static final String VER = "ver";
+
+    /**
+     * Name property.
+     */
+    @VisibleForTesting
+    static final String NAME = "name";
+
+    /**
+     * Time property.
+     */
+    @VisibleForTesting
+    static final String TIME = "time";
+
+    /**
+     * iKey property.
+     */
+    @VisibleForTesting
+    static final String IKEY = "iKey";
+
+    /**
+     * Extension property.
+     */
+    @VisibleForTesting
+    static final String EXT = "ext";
+
+    /**
+     * Common schema version.
+     * Defaults to a specific value when creating new logs.
+     * Existing logs in storage can have a different value.
+     */
+    private String ver = "3.0";
+
+    /**
+     * Event name.
+     */
+    private String name;
+
+    /**
+     * An identifier used to identify applications or other logical groupings of events.
+     */
+    private String iKey;
+
+    /**
+     * Get common schema version.
+     *
+     * @return common schema version.
+     */
+    public String getVer() {
+        return ver;
+    }
+
+    /**
+     * Set common schema version.
+     *
+     * @param ver common schema version.
+     */
+    public void setVer(String ver) {
+        this.ver = ver;
+    }
+
+    /**
+     * Get event name.
+     *
+     * @return event name.
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Set event name.
+     *
+     * @param name event name.
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Get instrumentation key.
+     *
+     * @return instrumentation key.
+     */
+    public String getIKey() {
+        return iKey;
+    }
+
+    /**
+     * Set instrumentation key.
+     *
+     * @param iKey instrumentation key.
+     */
+    public void setIKey(String iKey) {
+        this.iKey = iKey;
+    }
+
+    @Override
+    public void read(JSONObject object) throws JSONException {
+
+        /* Override abstract log JSON since it's Common Schema and not App Center Schema. */
+        setVer(object.getString(VER));
+        setName(object.getString(NAME));
+        setTimestamp(JSONDateUtils.toDate(object.getString(TIME)));
+        setIKey(object.getString(IKEY));
+    }
+
+    @Override
+    public void write(JSONStringer writer) throws JSONException {
+
+        /* Override abstract log JSON since it's Common Schema and not App Center Schema. */
+        writer.key(VER).value(getVer());
+        writer.key(NAME).value(getName());
+        writer.key(TIME).value(JSONDateUtils.toString(getTimestamp()));
+        writer.key(IKEY).value(getIKey());
+    }
+}
---
+++ sdk/appcenter/src/main/java/com/microsoft/appcenter/ingestion/models/one/DeviceExtension.java
@@ -0,0 +1,20 @@
+package com.microsoft.appcenter.ingestion.models.one;
+
+import com.microsoft.appcenter.ingestion.models.Model;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONStringer;
+
+public class DeviceExtension implements Model {
+
+    @Override
+    public void read(JSONObject object) throws JSONException {
+
+    }
+
+    @Override
+    public void write(JSONStringer writer) throws JSONException {
+
+    }
+}
