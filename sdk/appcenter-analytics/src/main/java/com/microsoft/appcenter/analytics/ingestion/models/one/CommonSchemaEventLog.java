aa5c1eff3e5228c634b9e3924d012e146fa0b1c4
Guillaume Perrot <guperrot@microsoft.com>
2018-05-21 17:19:25 -0700

    Add missing tests and refactoring

--- sdk/appcenter-analytics/src/main/java/com/microsoft/appcenter/analytics/ingestion/models/one/CommonSchemaEventLog.java
+++ sdk/appcenter-analytics/src/main/java/com/microsoft/appcenter/analytics/ingestion/models/one/CommonSchemaEventLog.java
@@ -2,29 +2,18 @@ package com.microsoft.appcenter.analytics.ingestion.models.one;
 
 import com.microsoft.appcenter.ingestion.models.one.CommonSchemaLog;
 
-import org.json.JSONException;
-import org.json.JSONObject;
-import org.json.JSONStringer;
-
 /**
  * Event log.
  */
 public class CommonSchemaEventLog extends CommonSchemaLog {
 
+    /**
+     * Type property.
+     */
     public static final String TYPE = "commonSchemaEvent";
 
     @Override
     public String getType() {
         return TYPE;
-    }
-
-    @Override
-    public void read(JSONObject object) throws JSONException {
-        super.read(object);
-    }
-
-    @Override
-    public void write(JSONStringer writer) throws JSONException {
-        super.write(writer);
     }
 }
