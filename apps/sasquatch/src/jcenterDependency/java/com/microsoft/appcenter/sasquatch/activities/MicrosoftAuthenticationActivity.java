92fee4b8d5a7ac4dde29144bdb343a7f27d9ce9d
Ivan Matkov <v-ivmatk@microsoft.com>
2018-08-03 20:19:14 +0300

    Fix lint error

--- apps/sasquatch/src/jcenterDependency/java/com/microsoft/appcenter/sasquatch/activities/MicrosoftAuthenticationActivity.java
+++ apps/sasquatch/src/jcenterDependency/java/com/microsoft/appcenter/sasquatch/activities/MicrosoftAuthenticationActivity.java
@@ -2,6 +2,9 @@ package com.microsoft.appcenter.sasquatch.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.microsoft.appcenter.sasquatch.R;
 
 /**
  * TODO: Remove this once new APIs available in jCenter.
@@ -11,6 +14,13 @@ public class MicrosoftAuthenticationActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_msa_auth);
+    }
+
+    public void onLoginClick(View view) {
+    }
+
+    public void onRefreshClick(View view) {
     }
 }
 
