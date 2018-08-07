5fe0b2f9d38ec5a25a95c35db367675c4fdb4231
Ivan Matkov <v-ivmatk@microsoft.com>
2018-08-07 18:20:18 +0300

    Fix NPE on recreate launch activity before full initialization

--- sdk/appcenter-distribute/src/test/java/com/microsoft/appcenter/distribute/DistributeTest.java
+++ sdk/appcenter-distribute/src/test/java/com/microsoft/appcenter/distribute/DistributeTest.java
@@ -1,8 +1,12 @@
 package com.microsoft.appcenter.distribute;
 
+import android.content.ComponentName;
+import android.content.Intent;
+
 import com.microsoft.appcenter.distribute.ingestion.models.DistributionStartSessionLog;
 import com.microsoft.appcenter.distribute.ingestion.models.json.DistributionStartSessionLogFactory;
 import com.microsoft.appcenter.ingestion.models.json.LogFactory;
+import com.microsoft.appcenter.utils.storage.StorageHelper;
 
 import org.junit.Test;
 
@@ -11,6 +15,10 @@ import java.util.Map;
 import static junit.framework.Assert.assertSame;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.anyInt;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class DistributeTest extends AbstractDistributeTest {
 
@@ -25,5 +33,25 @@ public class DistributeTest extends AbstractDistributeTest {
         assertNotNull(factories);
         assertTrue(factories.remove(DistributionStartSessionLog.TYPE) instanceof DistributionStartSessionLogFactory);
         assertTrue(factories.isEmpty());
+    }
+
+    @Test
+    public void recreateLauncherActivityBeforeFullInitialization() {
+
+        /* PreferencesStorage isn't initialized yet. */
+        when(StorageHelper.PreferencesStorage.getInt(anyString(), anyInt()))
+                .thenThrow(new NullPointerException());
+
+        /* Our activity is launch one. */
+        Intent intent = mock(Intent.class);
+        when(mPackageManager.getLaunchIntentForPackage(anyString())).thenReturn(intent);
+        ComponentName componentName = mock(ComponentName.class);
+        when(intent.resolveActivity(mPackageManager)).thenReturn(componentName);
+        when(componentName.getClassName()).thenReturn(mActivity.getClass().getName());
+
+        /* Callback. */
+        Distribute.getInstance().onActivityCreated(mActivity, null);
+
+        /* No exceptions. */
     }
 }
