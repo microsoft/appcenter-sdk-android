740558413ce59de72cddc5eee431f736e5ea9ee0
Guillaume Perrot <guperrot@microsoft.com>
2018-08-07 17:24:09 -0700

    Fix jCenter variant

--- apps/sasquatch/src/jcenterDependency/java/com/microsoft/appcenter/sasquatch/activities/MSALoginActivity.java
+++ apps/sasquatch/src/jcenterDependency/java/com/microsoft/appcenter/sasquatch/activities/MSALoginActivity.java
@@ -1,7 +1,11 @@
 package com.microsoft.appcenter.sasquatch.activities;
 
 import android.app.Activity;
+import android.view.View;
 
 /* TODO remove when authentication released in jCenter. */
 public class MSALoginActivity extends Activity {
+
+    public void reset(View view) {
+    }
 }
