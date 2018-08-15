a2cf3e97b8106d3afb161454accfaf7daefe72ec
Guillaume Perrot <guperrot@microsoft.com>
2018-08-15 15:11:57 -0700

    Align method name with apple sdk

--- sdk/appcenter-analytics/src/test/java/com/microsoft/appcenter/analytics/AuthenticationProviderTest.java
+++ sdk/appcenter-analytics/src/test/java/com/microsoft/appcenter/analytics/AuthenticationProviderTest.java
@@ -57,12 +57,12 @@ public class AuthenticationProviderTest {
     public void acquireTokenAsyncInvalidToken() {
         AuthenticationProvider.TokenProvider tokenProvider = mock(AuthenticationProvider.TokenProvider.class);
         ArgumentCaptor<AuthenticationProvider.AuthenticationCallback> callback = ArgumentCaptor.forClass(AuthenticationProvider.AuthenticationCallback.class);
-        doNothing().when(tokenProvider).getToken(anyString(), callback.capture());
+        doNothing().when(tokenProvider).acquireToken(anyString(), callback.capture());
         AuthenticationProvider authenticationProvider = new AuthenticationProvider(MSA_COMPACT, "key", tokenProvider);
 
         /* Verify acquireTokenAsync. */
         authenticationProvider.acquireTokenAsync();
-        verify(tokenProvider).getToken(eq("key"), any(AuthenticationProvider.AuthenticationCallback.class));
+        verify(tokenProvider).acquireToken(eq("key"), any(AuthenticationProvider.AuthenticationCallback.class));
 
         /* When callback parameters are invalid, don't update cache. */
         callback.getValue().onAuthenticationResult(null, new Date());
@@ -80,12 +80,12 @@ public class AuthenticationProviderTest {
     public void acquireTokenAsyncInvalidDate() {
         AuthenticationProvider.TokenProvider tokenProvider = mock(AuthenticationProvider.TokenProvider.class);
         ArgumentCaptor<AuthenticationProvider.AuthenticationCallback> callback = ArgumentCaptor.forClass(AuthenticationProvider.AuthenticationCallback.class);
-        doNothing().when(tokenProvider).getToken(anyString(), callback.capture());
+        doNothing().when(tokenProvider).acquireToken(anyString(), callback.capture());
         AuthenticationProvider authenticationProvider = new AuthenticationProvider(MSA_COMPACT, "key", tokenProvider);
 
         /* Verify acquireTokenAsync. */
         authenticationProvider.acquireTokenAsync();
-        verify(tokenProvider).getToken(eq("key"), any(AuthenticationProvider.AuthenticationCallback.class));
+        verify(tokenProvider).acquireToken(eq("key"), any(AuthenticationProvider.AuthenticationCallback.class));
 
         /* When callback parameters are invalid, don't update cache. */
         callback.getValue().onAuthenticationResult("test", null);
@@ -103,12 +103,12 @@ public class AuthenticationProviderTest {
     public void acquireTokenAsyncValid() {
         AuthenticationProvider.TokenProvider tokenProvider = mock(AuthenticationProvider.TokenProvider.class);
         ArgumentCaptor<AuthenticationProvider.AuthenticationCallback> callback = ArgumentCaptor.forClass(AuthenticationProvider.AuthenticationCallback.class);
-        doNothing().when(tokenProvider).getToken(anyString(), callback.capture());
+        doNothing().when(tokenProvider).acquireToken(anyString(), callback.capture());
         AuthenticationProvider authenticationProvider = new AuthenticationProvider(MSA_COMPACT, "key", tokenProvider);
 
         /* Verify acquireTokenAsync. */
         authenticationProvider.acquireTokenAsync();
-        verify(tokenProvider).getToken(eq("key"), any(AuthenticationProvider.AuthenticationCallback.class));
+        verify(tokenProvider).acquireToken(eq("key"), any(AuthenticationProvider.AuthenticationCallback.class));
 
         /* When callback parameters are valid update cache. */
         long freshDate = System.currentTimeMillis() + 15 * 60 * 1000;
@@ -136,7 +136,7 @@ public class AuthenticationProviderTest {
         authenticationProvider.acquireTokenAsync();
         Date expiryDate = mock(Date.class);
         when(expiryDate.getTime()).thenReturn(System.currentTimeMillis() + 15 * 60 * 1000);
-        verify(tokenProvider).getToken(anyString(), callback.capture());
+        verify(tokenProvider).acquireToken(anyString(), callback.capture());
         callback.getValue().onAuthenticationResult("test", expiryDate);
         verifyStatic();
         TicketCache.putTicket(eq(authenticationProvider.getTicketKeyHash()), eq("d:test"));
@@ -151,7 +151,7 @@ public class AuthenticationProviderTest {
         when(expiryDate.getTime()).thenReturn(System.currentTimeMillis() + 9 * 60 * 1000);
         authenticationProvider.checkTokenExpiry();
         verify(authenticationProvider).acquireTokenAsync();
-        verify(tokenProvider).getToken(anyString(), callback.capture());
+        verify(tokenProvider).acquireToken(anyString(), callback.capture());
 
         /* When checking a second time if expired before token provider called back. */
         reset(authenticationProvider);
@@ -160,7 +160,7 @@ public class AuthenticationProviderTest {
         verify(authenticationProvider).acquireTokenAsync();
 
         /* Then we don't call back again. */
-        verify(tokenProvider, never()).getToken(anyString(), callback.capture());
+        verify(tokenProvider, never()).acquireToken(anyString(), callback.capture());
 
         /* Call back after refresh. */
         callback.getValue().onAuthenticationResult("test", expiryDate);
@@ -174,7 +174,7 @@ public class AuthenticationProviderTest {
         reset(tokenProvider);
         authenticationProvider.checkTokenExpiry();
         verify(authenticationProvider).acquireTokenAsync();
-        verify(tokenProvider).getToken(anyString(), callback.capture());
+        verify(tokenProvider).acquireToken(anyString(), callback.capture());
         callback.getValue().onAuthenticationResult("test", expiryDate);
         verifyStatic(times(3));
         TicketCache.putTicket(eq(authenticationProvider.getTicketKeyHash()), eq("d:test"));
