7f4ee20dfbaee402c7c0d2dc4d9c7ec4c1a310f5
Guillaume Perrot <guperrot@microsoft.com>
2018-09-24 18:01:22 -0700

    Add a case to test filtering values out only in database

--- sdk/appcenter/src/androidTest/java/com/microsoft/appcenter/utils/storage/StorageHelperAndroidTest.java
+++ sdk/appcenter/src/androidTest/java/com/microsoft/appcenter/utils/storage/StorageHelperAndroidTest.java
@@ -23,6 +23,7 @@ import java.io.Serializable;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
@@ -288,6 +289,11 @@ public class StorageHelperAndroidTest {
         /* Test null value filter does not exclude anything, so returns the 2 logs. */
         scanner = databaseStorage.getScanner(null, null, "COL_STRING", null, false);
         assertEquals(2, scanner.getCount());
+
+        /* Test filtering only with the second key parameter to get only the second log. */
+        scanner = databaseStorage.getScanner(null, null, "COL_STRING", Collections.singletonList(value1.getAsString("COL_STRING")), false);
+        assertEquals(1, scanner.getCount());
+        assertContentValuesEquals(value2, scanner.iterator().next());
 
         /* Delete. */
         databaseStorage.delete(value1Id);
