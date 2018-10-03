4cfcfc6b0980443349c129c2063e39117a238505
Jae Lim <jaelim@microsoft.com>
2018-10-03 11:22:06 -0700

    Handle all supported type in event property

--- sdk/appcenter/src/main/java/com/microsoft/appcenter/ingestion/models/one/PartCUtils.java
+++ sdk/appcenter/src/main/java/com/microsoft/appcenter/ingestion/models/one/PartCUtils.java
@@ -1,5 +1,9 @@
 package com.microsoft.appcenter.ingestion.models.one;
 
+import com.microsoft.appcenter.ingestion.models.properties.BooleanTypedProperty;
+import com.microsoft.appcenter.ingestion.models.properties.DateTimeTypedProperty;
+import com.microsoft.appcenter.ingestion.models.properties.DoubleTypedProperty;
+import com.microsoft.appcenter.ingestion.models.properties.LongTypedProperty;
 import com.microsoft.appcenter.ingestion.models.properties.StringTypedProperty;
 import com.microsoft.appcenter.ingestion.models.properties.TypedProperty;
 import com.microsoft.appcenter.utils.AppCenterLog;
@@ -33,49 +37,60 @@ public class PartCUtils {
             dest.setData(data);
             for (TypedProperty property : properties) {
 
-                /* TODO handle other types and metadata. */
-                if (property instanceof StringTypedProperty) {
-
-                    /* Validate key not null. */
-                    String key = property.getName();
-                    if (key == null) {
-                        AppCenterLog.warn(LOG_TAG, "Property key cannot be null.");
-                        continue;
-                    }
-
-                    /* Validate value not null. */
-                    String value = ((StringTypedProperty) property).getValue();
-                    if (value == null) {
-                        AppCenterLog.warn(LOG_TAG, "Value of property with key '" + key + "' cannot be null.");
-                        continue;
-                    }
-
-                    /* Validate key is not Part B. */
-                    if (Data.BASE_DATA.equals(key) || Data.BASE_DATA_TYPE.equals(key)) {
-                        AppCenterLog.warn(LOG_TAG, "Property key '" + key + "' is reserved.");
-                        continue;
-                    }
-
-                    /* Split property name by dot. */
-                    String[] keys = key.split("\\.", -1);
-                    int lastIndex = keys.length - 1;
-                    JSONObject destProperties = data.getProperties();
-                    for (int i = 0; i < lastIndex; i++) {
-                        JSONObject subObject = destProperties.optJSONObject(keys[i]);
-                        if (subObject == null) {
-                            if (destProperties.has(keys[i])) {
-                                AppCenterLog.warn(LOG_TAG, "Property key '" + keys[i] + "' already has a value, the old value will be overridden.");
-                            }
-                            subObject = new JSONObject();
-                            destProperties.put(keys[i], subObject);
-                        }
-                        destProperties = subObject;
-                    }
-                    if (destProperties.has(keys[lastIndex])) {
-                        AppCenterLog.warn(LOG_TAG, "Property key '" + keys[lastIndex] + "' already has a value, the old value will be overridden.");
-                    }
-                    destProperties.put(keys[lastIndex], value);
+                /* Validate key not null. */
+                String key = property.getName();
+                if (key == null) {
+                    AppCenterLog.warn(LOG_TAG, "Property key cannot be null.");
+                    continue;
                 }
+
+                /* Validate key is not Part B. */
+                if (Data.BASE_DATA.equals(key) || Data.BASE_DATA_TYPE.equals(key)) {
+                    AppCenterLog.warn(LOG_TAG, "Property key '" + key + "' is reserved.");
+                    continue;
+                }
+
+                /* Get value from property. */
+                Object value;
+                if (property instanceof StringTypedProperty) {
+                    value = ((StringTypedProperty) property).getValue();
+                } else if (property instanceof LongTypedProperty) {
+                    value = ((LongTypedProperty) property).getValue();
+                } else if (property instanceof DoubleTypedProperty) {
+                    value = ((DoubleTypedProperty) property).getValue();
+                } else if (property instanceof DateTimeTypedProperty) {
+                    value = ((DateTimeTypedProperty) property).getValue();
+                } else if (property instanceof BooleanTypedProperty) {
+                    value = ((BooleanTypedProperty) property).getValue();
+                } else {
+                    value = null;
+                }
+
+                /* Validate value not null. */
+                if (value == null) {
+                    AppCenterLog.warn(LOG_TAG, "Value of property with key '" + key + "' cannot be null.");
+                    continue;
+                }
+
+                /* Split property name by dot. */
+                String[] keys = key.split("\\.", -1);
+                int lastIndex = keys.length - 1;
+                JSONObject destProperties = data.getProperties();
+                for (int i = 0; i < lastIndex; i++) {
+                    JSONObject subObject = destProperties.optJSONObject(keys[i]);
+                    if (subObject == null) {
+                        if (destProperties.has(keys[i])) {
+                            AppCenterLog.warn(LOG_TAG, "Property key '" + keys[i] + "' already has a value, the old value will be overridden.");
+                        }
+                        subObject = new JSONObject();
+                        destProperties.put(keys[i], subObject);
+                    }
+                    destProperties = subObject;
+                }
+                if (destProperties.has(keys[lastIndex])) {
+                    AppCenterLog.warn(LOG_TAG, "Property key '" + keys[lastIndex] + "' already has a value, the old value will be overridden.");
+                }
+                destProperties.put(keys[lastIndex], value);
             }
         } catch (JSONException ignore) {
 
