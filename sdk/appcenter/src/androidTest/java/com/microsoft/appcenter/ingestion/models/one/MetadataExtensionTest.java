896bd560a4745a31dc1ec8dfe58b6f37cd4ca73d
Jae Lim <jaelim@microsoft.com>
2018-10-05 12:26:50 -0700

    Add MetadataExtension unit tests

---
+++ sdk/appcenter/src/androidTest/java/com/microsoft/appcenter/ingestion/models/one/MetadataExtensionTest.java
@@ -0,0 +1,32 @@
+package com.microsoft.appcenter.ingestion.models.one;
+
+import com.microsoft.appcenter.test.TestUtils;
+
+import org.json.JSONException;
+import org.junit.Test;
+
+import static com.microsoft.appcenter.test.TestUtils.checkEquals;
+import static com.microsoft.appcenter.test.TestUtils.checkNotEquals;
+
+public class MetadataExtensionTest {
+
+    @Test
+    public void compareDifferentType() {
+        TestUtils.compareSelfNullClass(new MetadataExtension());
+    }
+
+    @Test
+    public void equalsHashCode() throws JSONException {
+
+        /* Empty objects. */
+        MetadataExtension a = new MetadataExtension();
+        MetadataExtension b = new MetadataExtension();
+        checkEquals(a, b);
+
+        /* Properties. */
+        a.getMetadata().put("a", "b");
+        checkNotEquals(a, b);
+        b.getMetadata().put("a", "b");
+        checkEquals(a, b);
+    }
+}
