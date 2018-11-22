2744c8e3940e870a8d5d92e020f97730d9ea6d74
Guillaume Perrot <guperrot@microsoft.com>
2018-11-21 16:20:51 -0800

    Add unit test to cover huge payload logging
    It was covered accidentally in unrelated test and not guaranteed.

--- sdk/appcenter/src/test/java/com/microsoft/appcenter/http/DefaultHttpClientTest.java
+++ sdk/appcenter/src/test/java/com/microsoft/appcenter/http/DefaultHttpClientTest.java
@@ -801,4 +801,76 @@ public class DefaultHttpClientTest {
             }
         }));
     }
+
+    @Test
+    public void sendGzipWithHugeTextAndVerboseLogging() throws Exception {
+
+        /* Mock verbose logging. */
+        mockStatic(AppCenterLog.class);
+        when(AppCenterLog.getLogLevel()).thenReturn(Log.VERBOSE);
+
+        /* Configure mock HTTP. */
+        String urlString = "http://mock";
+        URL url = mock(URL.class);
+        whenNew(URL.class).withArguments(urlString).thenReturn(url);
+        HttpsURLConnection urlConnection = mock(HttpsURLConnection.class);
+        when(url.openConnection()).thenReturn(urlConnection);
+        when(urlConnection.getResponseCode()).thenReturn(200);
+        ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+        when(urlConnection.getOutputStream()).thenReturn(buffer);
+        when(urlConnection.getInputStream()).thenReturn(new ByteArrayInputStream("OK".getBytes()));
+
+        /* Long mock payload. */
+        StringBuilder payloadBuilder = new StringBuilder();
+        for (int i = 0; i < 8000; i++) {
+            payloadBuilder.append('a');
+        }
+        final String payload = payloadBuilder.toString();
+
+        /* Compress payload for verification. */
+        ByteArrayOutputStream gzipBuffer = new ByteArrayOutputStream(payload.length());
+        GZIPOutputStream gzipStream = new GZIPOutputStream(gzipBuffer);
+        gzipStream.write(payload.getBytes("UTF-8"));
+        gzipStream.close();
+        byte[] compressedBytes = gzipBuffer.toByteArray();
+
+        /* Configure API client. */
+        HttpClient.CallTemplate callTemplate = mock(HttpClient.CallTemplate.class);
+        when(callTemplate.buildRequestBody()).thenReturn(payload);
+        DefaultHttpClient httpClient = new DefaultHttpClient();
+
+        /* Test calling code. */
+        String appSecret = UUIDUtils.randomUUID().toString();
+        UUID installId = UUIDUtils.randomUUID();
+        Map<String, String> headers = new HashMap<>();
+        headers.put("Content-Type", "custom");
+        ServiceCallback serviceCallback = mock(ServiceCallback.class);
+        mockCall();
+        httpClient.callAsync(urlString, METHOD_POST, headers, callTemplate, serviceCallback);
+        verify(serviceCallback).onCallSucceeded("OK");
+        verifyNoMoreInteractions(serviceCallback);
+        verify(urlConnection).setRequestProperty("Content-Type", "custom");
+
+        /* Also verify content type was set only once, json not applied. */
+        verify(urlConnection).setRequestProperty(eq("Content-Type"), anyString());
+        verify(urlConnection).setRequestProperty("Content-Encoding", "gzip");
+        verify(urlConnection).setRequestMethod("POST");
+        verify(urlConnection).setDoOutput(true);
+        verify(urlConnection).disconnect();
+        verify(callTemplate).onBeforeCalling(eq(url), anyMapOf(String.class, String.class));
+        verify(callTemplate).buildRequestBody();
+        httpClient.close();
+
+        /* Verify payload compressed. */
+        assertArrayEquals(compressedBytes, buffer.toByteArray());
+
+        /* Check payload logged but not as JSON since different content type. */
+        verifyStatic();
+        AppCenterLog.verbose(anyString(), argThat(new ArgumentMatcher<String>() {
+            @Override
+            public boolean matches(Object argument) {
+                return argument.toString().contains(payload);
+            }
+        }));
+    }
 }
