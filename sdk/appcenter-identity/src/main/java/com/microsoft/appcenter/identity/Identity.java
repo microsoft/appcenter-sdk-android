1384b76232c96436121368bb70372016cf71c068
Benjamin Scholtysik (Reimold) <bereimol@microsoft.com>
2019-01-16 12:19:44 -0800

    Add empty module for App Center Identity

---
+++ sdk/appcenter-identity/src/main/java/com/microsoft/appcenter/identity/Identity.java
@@ -0,0 +1,134 @@
+package com.microsoft.appcenter.identity;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.util.Log;
+
+import com.microsoft.appcenter.AbstractAppCenterService;
+import com.microsoft.appcenter.AppCenter;
+import com.microsoft.appcenter.channel.Channel;
+import com.microsoft.appcenter.http.DefaultHttpClient;
+import com.microsoft.appcenter.http.HttpClient;
+import com.microsoft.appcenter.http.HttpClientNetworkStateHandler;
+import com.microsoft.appcenter.http.ServiceCallback;
+import com.microsoft.appcenter.utils.AppCenterLog;
+import com.microsoft.appcenter.utils.NetworkStateHelper;
+import com.microsoft.appcenter.utils.UUIDUtils;
+import com.microsoft.appcenter.utils.async.AppCenterFuture;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.Map;
+import java.util.Random;
+
+import static com.microsoft.appcenter.http.DefaultHttpClient.METHOD_GET;
+
+/**
+ * identity service.
+ */
+public class Identity extends AbstractAppCenterService {
+
+    /**
+     * Name of the service.
+     */
+    private static final String SERVICE_NAME = "Identity";
+
+    /**
+     * TAG used in logging for Identity.
+     */
+    private static final String LOG_TAG = AppCenterLog.LOG_TAG + SERVICE_NAME;
+
+    /**
+     * Shared instance.
+     */
+    @SuppressLint("StaticFieldLeak")
+    private static Identity sInstance;
+
+    /**
+     * Application context.
+     */
+    private Context mContext;
+
+    /**
+     * Get shared instance.
+     *
+     * @return shared instance.
+     */
+    @SuppressWarnings("WeakerAccess")
+    public static synchronized Identity getInstance() {
+        if (sInstance == null) {
+            sInstance = new Identity();
+        }
+        return sInstance;
+    }
+
+    /**
+     * Check whether Identity service is enabled or not.
+     *
+     * @return future with result being <code>true</code> if enabled, <code>false</code> otherwise.
+     * @see AppCenterFuture
+     */
+    @SuppressWarnings("unused")
+    public static AppCenterFuture<Boolean> isEnabled() {
+        return getInstance().isInstanceEnabledAsync();
+    }
+
+    /**
+     * Enable or disable Identity service.
+     *
+     * @param enabled <code>true</code> to enable, <code>false</code> to disable.
+     * @return future with null result to monitor when the operation completes.
+     */
+    @SuppressWarnings("unused")
+    public static AppCenterFuture<Void> setEnabled(boolean enabled) {
+        return getInstance().setInstanceEnabledAsync(enabled);
+    }
+
+    @Override
+    public synchronized void onStarted(@NonNull Context context, @NonNull Channel channel, String appSecret, String transmissionTargetToken, boolean startedFromApp) {
+        mContext = context;
+        super.onStarted(context, channel, appSecret, transmissionTargetToken, startedFromApp);
+    }
+
+    /**
+     * React to enable state change.
+     *
+     * @param enabled current state.
+     */
+    @Override
+    protected synchronized void applyEnabledState(boolean enabled) {
+        if (enabled) {
+            //TODO: add implementation.
+        }
+        else if (mHttpClient != null) {
+             //TODO: add implementation.
+        }
+    }
+
+    @Override
+    protected String getGroupName() {
+        return null;
+    }
+
+    @Override
+    public String getServiceName() {
+        return SERVICE_NAME;
+    }
+
+    @Override
+    protected String getLoggerTag() {
+        return LOG_TAG;
+    }
+}
