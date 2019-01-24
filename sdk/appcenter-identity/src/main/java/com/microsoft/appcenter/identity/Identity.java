ea474bd159ad12a567144ac6ed61617e866b8e7d
Guillaume Perrot <guperrot@microsoft.com>
2019-01-23 17:07:55 -0800

    Update behavior/cache/download logic according to initial discussions

--- sdk/appcenter-identity/src/main/java/com/microsoft/appcenter/identity/Identity.java
+++ sdk/appcenter-identity/src/main/java/com/microsoft/appcenter/identity/Identity.java
@@ -2,6 +2,7 @@ package com.microsoft.appcenter.identity;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.os.Build;
 import android.support.annotation.NonNull;
 import android.support.annotation.VisibleForTesting;
 
@@ -49,13 +50,15 @@ public class Identity extends AbstractAppCenterService {
     /**
      * Constant marking event of the identity group.
      */
-    static final String IDENTITY_GROUP = "group_identity";
+    private static final String IDENTITY_GROUP = "group_identity";
 
     private static final String CONFIG_URL = "https://mobilecentersdkdev.blob.core.windows.net/identity/%s.json";
 
     private static final String FILE_PATH = "appcenter/identity/config.json";
 
-    private static final String STORAGE_ETAG_KEY = SERVICE_NAME + ".configFileETag";
+    private static final String PREFERENCE_ETAG_KEY = SERVICE_NAME + ".configFileETag";
+
+    private static final String HEADER_E_TAG = "ETag";
 
     private static final String HEADER_IF_NONE_MATCH = "If-None-Match";
 
@@ -75,6 +78,9 @@ public class Identity extends AbstractAppCenterService {
      */
     private String mAppSecret;
 
+    /**
+     * Authentication client.
+     */
     private PublicClientApplication mAuthenticationClient;
 
     /**
@@ -138,27 +144,23 @@ public class Identity extends AbstractAppCenterService {
     @Override
     protected synchronized void applyEnabledState(boolean enabled) {
         if (enabled) {
+
+            /* Make module no-op if running on API level < 21, TODO this check can be removed when next msal lib released. */
+            int minApiLevel = Build.VERSION_CODES.LOLLIPOP;
+            if (Build.VERSION.SDK_INT <= minApiLevel) {
+                AppCenterLog.error(LOG_TAG, "Identity requires API level " + minApiLevel);
+                return;
+            }
             if (mAppSecret != null) {
 
-                /* Use cached config file if available. */
-                File configFile = getConfigFile();
-                if (configFile.exists()) {
-                    try {
-                        // TODO init auth client from file, requires msal SDK with patch
-                        // or parse JSON and create manually like in download method.
-                    } catch (RuntimeException e) {
-                        AppCenterLog.warn(LOG_TAG, "Cannot used cached config.", e);
-                        SharedPreferencesManager.remove(STORAGE_ETAG_KEY);
-                    }
-                }
-
-                /* Either way, refresh configuration file in background. */
+                /* Download fresh configuration first instead of using cache directly. */
                 downloadConfiguration();
             } else {
                 AppCenterLog.error(LOG_TAG, "Identity needs to be started with an application secret.");
             }
         } else {
             //TODO: cancel tasks.
+            clearCache();
         }
     }
 
@@ -178,9 +180,11 @@ public class Identity extends AbstractAppCenterService {
     }
 
     private void downloadConfiguration() {
+
+        /* Configure http call to download the configuration. Add ETag if we have a cached entry. */
         HttpClient httpClient = createHttpClient(mContext);
         Map<String, String> headers = new HashMap<>();
-        String eTag = SharedPreferencesManager.getString(STORAGE_ETAG_KEY);
+        String eTag = SharedPreferencesManager.getString(PREFERENCE_ETAG_KEY);
         if (eTag != null) {
             headers.put(HEADER_IF_NONE_MATCH, eTag);
         }
@@ -204,13 +208,13 @@ public class Identity extends AbstractAppCenterService {
 
             @Override
             public void onCallSucceeded(String payload, Map<String, String> headers) {
-                processDownloadedConfig(payload, headers.get("ETag"));
+                processDownloadedConfig(payload, headers.get(HEADER_E_TAG));
             }
 
             @Override
             public void onCallFailed(Exception e) {
                 if (e instanceof HttpException && ((HttpException) e).getStatusCode() == 304) {
-                    AppCenterLog.info(LOG_TAG, "Identify configuration didn't change.");
+                    loadConfigurationFromCache();
                 } else {
                     AppCenterLog.error(LOG_TAG, "Cannot load identity configuration from the server.", e);
                 }
@@ -218,10 +222,28 @@ public class Identity extends AbstractAppCenterService {
         });
     }
 
+    private synchronized void processDownloadedConfig(String payload, String eTag) {
+        if (initAuthenticationClient(payload)) {
+            saveConfigFile(payload, eTag);
+        }
+    }
 
-    private void processDownloadedConfig(String payload, String eTag) {
+    private synchronized void loadConfigurationFromCache() {
+        AppCenterLog.info(LOG_TAG, "Identify configuration didn't change, will use cache.");
+        boolean configurationSucceeded = false;
+        String configurationPayload = FileManager.read(getConfigFile());
+        if (configurationPayload != null) {
+            configurationSucceeded = initAuthenticationClient(configurationPayload);
+        }
+        if (!configurationSucceeded) {
+            clearCache();
+            downloadConfiguration();
+        }
+    }
+
+    private synchronized boolean initAuthenticationClient(String configurationPayload) {
         try {
-            JSONObject configuration = new JSONObject(payload);
+            JSONObject configuration = new JSONObject(configurationPayload);
             String identityScope = configuration.getString("identity_scope");
             String clientId = configuration.getString("client_id");
             String authorityUrl = null;
@@ -234,17 +256,24 @@ public class Identity extends AbstractAppCenterService {
                 }
             }
             if (authorityUrl != null) {
-                // TODO handle thread safety with disable and flows being executed.
+
+                // TODO use file to allow redirect_uri to be passed, need next msal version.
                 mAuthenticationClient = new PublicClientApplication(mContext, clientId, authorityUrl);
                 mIdentityScope = identityScope;
-                AppCenterLog.info(LOG_TAG, "Identity service configured successfully.");
-                saveConfigFile(payload, eTag);
             } else {
                 throw new IllegalStateException("Cannot find a b2c authority configured to be the default.");
             }
+            AppCenterLog.info(LOG_TAG, "Identity service configured successfully.");
+            return true;
         } catch (JSONException | RuntimeException e) {
-            AppCenterLog.error(LOG_TAG, "The download configuration is invalid.", e);
+            AppCenterLog.error(LOG_TAG, "The configuration is invalid.", e);
         }
+        return false;
+    }
+
+    @NonNull
+    private File getConfigFile() {
+        return new File(mContext.getFilesDir(), FILE_PATH);
     }
 
     private void saveConfigFile(String payload, String eTag) {
@@ -252,14 +281,15 @@ public class Identity extends AbstractAppCenterService {
         FileManager.mkdir(file.getParent());
         try {
             FileManager.write(file, payload);
-            SharedPreferencesManager.putString(STORAGE_ETAG_KEY, eTag);
+            SharedPreferencesManager.putString(PREFERENCE_ETAG_KEY, eTag);
         } catch (IOException e) {
             AppCenterLog.warn(LOG_TAG, "Failed to cache identity configuration.", e);
         }
+        AppCenterLog.debug(LOG_TAG, "Identity configuration saved in cache.");
     }
 
-    @NonNull
-    private File getConfigFile() {
-        return new File(mContext.getFilesDir(), FILE_PATH);
+    private void clearCache() {
+        SharedPreferencesManager.remove(PREFERENCE_ETAG_KEY);
+        FileManager.delete(getConfigFile());
     }
 }
