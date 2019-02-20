5cbba35c1ecf66dddf0ce777ec27e3166e99ad98
Anna Kocheshkova <v-annkoc@microsoft.com>
2019-02-20 18:06:40 +0300

    Fix import

--- sdk/appcenter-identity/src/main/java/com/microsoft/appcenter/identity/storage/PreferenceTokenStorage.java
+++ sdk/appcenter-identity/src/main/java/com/microsoft/appcenter/identity/storage/PreferenceTokenStorage.java
@@ -1,6 +1,7 @@
 package com.microsoft.appcenter.identity.storage;
 
 import android.content.Context;
+import android.support.annotation.NonNull;
 import android.support.annotation.VisibleForTesting;
 
 import com.microsoft.appcenter.utils.context.AuthTokenContext;
