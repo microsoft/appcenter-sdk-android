a7849d1bfb7cd4bee83031db2e46d2f26df0edaf
Guillaume Perrot <guperrot@microsoft.com>
2019-03-14 13:35:17 -0700

    Add copyright to Storage

--- sdk/appcenter-storage/src/main/java/com/microsoft/appcenter/storage/client/CosmosDb.java
+++ sdk/appcenter-storage/src/main/java/com/microsoft/appcenter/storage/client/CosmosDb.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
 package com.microsoft.appcenter.storage.client;
 
 import com.microsoft.appcenter.http.HttpClient;
