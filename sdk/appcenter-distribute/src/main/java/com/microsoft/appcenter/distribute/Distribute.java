339af934fa39219226adc61e3a2e6d7b0f3ce7b2
Ryan Thomas <Ryan.Thomas@rhinobyte.com>
2019-03-15 12:56:57 -0500

    DebuggableBuildSupport - Update the appcenter-distribute Distribute.java module to support running in DEBUGGABLE builds if the consumer explicitly opts into doing so via a new Distribute.setEnabledForDebuggableBuild(..) method call

--- sdk/appcenter-distribute/src/main/java/com/microsoft/appcenter/distribute/Distribute.java
+++ sdk/appcenter-distribute/src/main/java/com/microsoft/appcenter/distribute/Distribute.java
@@ -298,6 +298,14 @@ public class Distribute extends AbstractAppCenterService {
     private SharedPreferences mMobileCenterPreferenceStorage;
 
     /**
+     * Flag to track whether the distribute feature can be used in a debuggable build.
+     * Flag is false by default.
+     *
+     * Call setEnabledForDebuggableBuild(boolean) to adjust.
+     */
+    private boolean mEnabledForDebuggableBuild;
+
+    /**
      * Init.
      */
     private Distribute() {
@@ -375,6 +383,16 @@ public class Distribute extends AbstractAppCenterService {
     @SuppressWarnings("WeakerAccess")
     public static void setListener(DistributeListener listener) {
         getInstance().setInstanceListener(listener);
+    }
+
+    /**
+     * Change whether the distribute service can be used within a DEBUGGABLE build.
+     *
+     * @param enabled <code>true</code> to enable, <code>false</code> to disable.
+     */
+    @SuppressWarnings("WeakerAccess")
+    public static void setEnabledForDebuggableBuild(boolean enabled) {
+        getInstance().setInstanceEnabledForDebuggableBuild(enabled);
     }
 
     /**
@@ -595,6 +613,13 @@ public class Distribute extends AbstractAppCenterService {
     }
 
     /**
+     * Implements {@link #setEnabledForDebuggableBuild(Boolean)}.
+     */
+    private synchronized void setInstanceEnabledForDebuggableBuild(boolean enabled) {
+        mEnabledForDebuggableBuild = enabled;
+    }
+
+    /**
      * Cancel everything.
      */
     private synchronized void cancelPreviousTasks() {
@@ -638,8 +663,8 @@ public class Distribute extends AbstractAppCenterService {
         if (mPackageInfo != null && mForegroundActivity != null && !mWorkflowCompleted && isInstanceEnabled()) {
 
             /* Don't go any further it this is a debug app. */
-            if ((mContext.getApplicationInfo().flags & FLAG_DEBUGGABLE) == FLAG_DEBUGGABLE) {
-                AppCenterLog.info(LOG_TAG, "Not checking in app updates in debug.");
+            if ((mContext.getApplicationInfo().flags & FLAG_DEBUGGABLE) == FLAG_DEBUGGABLE && !mEnabledForDebuggableBuild) {
+                AppCenterLog.info(LOG_TAG, "Not checking for in app updates in debuggable build.");
                 mWorkflowCompleted = true;
                 return;
             }
