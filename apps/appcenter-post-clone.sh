7e50066ab31fcb59210ec1882ba5b3e415f68558
Guillaume Perrot <guperrot@microsoft.com>
2019-03-21 14:14:09 -0700

    Fix appcenter build on INT

--- apps/appcenter-post-clone.sh
+++ apps/appcenter-post-clone.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 echo "Executing post clone script in `pwd`"
 echo "dimension.dependency=$DIMENSION_DEPENDENCY" >> $APPCENTER_SOURCE_DIRECTORY/local.properties
+echo "azure.artifacts.gradle.access.token=$AZURE_ARTIFACTS_GRADLE_ACCESS_TOKEN" >> $APPCENTER_SOURCE_DIRECTORY/local.properties
