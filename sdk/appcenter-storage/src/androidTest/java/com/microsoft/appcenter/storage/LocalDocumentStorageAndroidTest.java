491d293bc7c22d4e686c8b8e98432c121e63b5f3
Angela Burton <angela.burton@microsoft.com>
2019-04-12 17:16:09 -0700

    Fix test.

--- sdk/appcenter-storage/src/androidTest/java/com/microsoft/appcenter/storage/LocalDocumentStorageAndroidTest.java
+++ sdk/appcenter-storage/src/androidTest/java/com/microsoft/appcenter/storage/LocalDocumentStorageAndroidTest.java
@@ -76,16 +76,16 @@ public class LocalDocumentStorageAndroidTest {
     @Test
     public void writeReadFail() {
         Document<String> document = new Document<>(TEST_VALUE, Constants.READONLY, ID);
-        mLocalDocumentStorage.writeOnline(mUserTableName, document, new WriteOptions());
+        mLocalDocumentStorage.writeOnline(USER_TABLE_NAME, document, new WriteOptions());
         assertFalse(document.failed());
 
         /* Pass incorrect class type to create a deserialization error.  */
-        Document<Integer> failedDocument = mLocalDocumentStorage.read(mUserTableName, Constants.READONLY, ID, Integer.class, new ReadOptions());
+        Document<Integer> failedDocument = mLocalDocumentStorage.read(USER_TABLE_NAME, Constants.READONLY, ID, Integer.class, new ReadOptions());
         assertNotNull(failedDocument);
         assertTrue(failedDocument.failed());
 
         /* Confirm document can still be correctly retrieved from the cache. */
-        Document<String> cachedDocument = mLocalDocumentStorage.read(mUserTableName, Constants.READONLY, ID, String.class, new ReadOptions());
+        Document<String> cachedDocument = mLocalDocumentStorage.read(USER_TABLE_NAME, Constants.READONLY, ID, String.class, new ReadOptions());
         assertNotNull(cachedDocument);
         assertFalse(cachedDocument.failed());
         assertEquals(document.getDocument(), cachedDocument.getDocument());
