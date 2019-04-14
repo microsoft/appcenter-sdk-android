aa3251484cbe81501603a039fbadae27145d8ba9
Anna Kocheshkova <v-annkoc@microsoft.com>
2019-04-14 13:27:04 +0300

    Added Time To Live option and fixed strings

--- apps/sasquatch/src/projectDependency/java/com/microsoft/appcenter/sasquatch/activities/NewUserDocumentActivity.java
+++ apps/sasquatch/src/projectDependency/java/com/microsoft/appcenter/sasquatch/activities/NewUserDocumentActivity.java
@@ -12,14 +12,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.microsoft.appcenter.sasquatch.R;
 import com.microsoft.appcenter.sasquatch.fragments.TypedPropertyFragment;
 import com.microsoft.appcenter.storage.Constants;
 import com.microsoft.appcenter.storage.Storage;
+import com.microsoft.appcenter.storage.models.BaseOptions;
 import com.microsoft.appcenter.storage.models.Document;
+import com.microsoft.appcenter.storage.models.WriteOptions;
 import com.microsoft.appcenter.utils.async.AppCenterConsumer;
 
 import java.util.ArrayList;
@@ -32,13 +37,43 @@ public class NewUserDocumentActivity extends AppCompatActivity {
     private final List<TypedPropertyFragment> mProperties = new ArrayList<>();
 
     private EditText mEditDocumentId;
+    private Spinner mTtlSpinner;
+    private WriteOptions mWriteOption = new WriteOptions(BaseOptions.DEFAULT_ONE_HOUR);
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_user_document);
         mEditDocumentId = findViewById(R.id.user_document_id);
+        mTtlSpinner = findViewById(R.id.ttl_spinner);
+        ArrayAdapter<String> typeAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, getResources().getStringArray(R.array.storage_ttls));
+        mTtlSpinner.setAdapter(typeAdapter);
+        mTtlSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                updateWriteOptions(position);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
         addProperty();
+    }
+
+    private void updateWriteOptions(int position) {
+        switch (position) {
+            case 1:
+                mWriteOption = WriteOptions.CreateNoCacheOption();
+                break;
+            case 2:
+                mWriteOption = new WriteOptions(2);
+                break;
+            case 3:
+                mWriteOption = WriteOptions.CreateInfiniteCacheOption();
+                break;
+        }
     }
 
     @Override
@@ -70,7 +105,7 @@ public class NewUserDocumentActivity extends AppCompatActivity {
             property.setGenericProperty(document);
         }
         String documentId = mEditDocumentId.getText().toString();
-        Storage.replace(Constants.USER, documentId, document, Map.class).thenAccept(new AppCenterConsumer<Document<Map>>() {
+        Storage.replace(Constants.USER, documentId, document, Map.class, mWriteOption).thenAccept(new AppCenterConsumer<Document<Map>>() {
             @Override
             public void accept(Document<Map> mapDocument) {
                 if (mapDocument.failed())
