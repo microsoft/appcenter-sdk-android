15e029485ff6de6569360347fb166b57a6097df5
Owen Niblock <me@owenniblock.com>
2019-05-22 13:55:47 +0100

    Removes the duplicate method entirely

--- sdk/appcenter/src/main/java/com/microsoft/appcenter/utils/InstrumentationRegistryHelper.java
+++ sdk/appcenter/src/main/java/com/microsoft/appcenter/utils/InstrumentationRegistryHelper.java
@@ -23,10 +23,6 @@ public class InstrumentationRegistryHelper {
      * @throws LinkageError          if the class, method is not found or does not match, typically no test dependencies in release.
      */
     public static Bundle getArguments() throws LinkageError {
-        return getArgumentsByReflection();
-    }
-
-    private static Bundle getArgumentsByReflection() {
         for (String location: locations) {
             try {
                 Class<?> aClass = Class.forName(location);
