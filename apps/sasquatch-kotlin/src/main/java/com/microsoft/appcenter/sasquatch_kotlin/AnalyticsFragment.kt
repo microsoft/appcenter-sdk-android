da65cfbbd508608c7fb60b10162ce81d96bd23d0
Ivan Matkov <v-ivmatk@microsoft.com>
2019-06-08 19:45:24 +0300

    Add navigation to kotlin sample app

---
+++ apps/sasquatch-kotlin/src/main/java/com/microsoft/appcenter/sasquatch_kotlin/AnalyticsFragment.kt
@@ -0,0 +1,18 @@
+package com.microsoft.appcenter.sasquatch_kotlin
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class AnalyticsFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_analytics, container, false)
+    }
+
+
+}
