5349dd6211e3c6355549a0c75b6827ed8acf3b63
Manu Chandel <machande@microsoft.com>
2019-09-10 18:33:55 -0700

    get authority url during init from config

--- sdk/appcenter-auth/src/main/java/com/microsoft/appcenter/auth/Auth.java
+++ sdk/appcenter-auth/src/main/java/com/microsoft/appcenter/auth/Auth.java
@@ -479,7 +479,7 @@ public class Auth extends AbstractAppCenterService implements NetworkStateHelper
                 throw new IllegalStateException("Cannot find a default b2c or aad authority configured to be the default.");
             }
             mAuthenticationClient = new PublicClientApplication(mContext, getConfigFile());
-            mAuthorityUrl = authorityUrl;
+            mAuthorityUrl = mAuthenticationClient.getConfiguration().getAuthorities().get(0).getAuthorityUri().toString();
             mIdentityScope = identityScope;
             AppCenterLog.info(LOG_TAG, "Auth service configured successfully.");
         } catch (JSONException | RuntimeException e) {
