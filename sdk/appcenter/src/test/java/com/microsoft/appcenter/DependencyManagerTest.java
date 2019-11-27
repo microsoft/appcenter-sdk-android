33a5e5ab047589170e94f88ea3c1d7f69d13dd23
Di Hei <dihei@microsoft.com>
2019-11-27 09:07:11 -0800

    Address feedback

--- sdk/appcenter/src/test/java/com/microsoft/appcenter/DependencyManagerTest.java
+++ sdk/appcenter/src/test/java/com/microsoft/appcenter/DependencyManagerTest.java
@@ -11,17 +11,14 @@ import android.os.Handler;
 import com.microsoft.appcenter.channel.DefaultChannel;
 import com.microsoft.appcenter.http.HttpClient;
 import com.microsoft.appcenter.http.HttpClientRetryer;
-import com.microsoft.appcenter.http.ServiceCall;
-import com.microsoft.appcenter.http.ServiceCallback;
 import com.microsoft.appcenter.ingestion.models.json.LogSerializer;
 
 import org.junit.Test;
 
-import java.util.Map;
-
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.eq;
 import static org.mockito.Matchers.isA;
+import static org.mockito.Mockito.mock;
 import static org.powermock.api.mockito.PowerMockito.verifyNew;
 
 public class DependencyManagerTest extends AbstractAppCenterTest {
@@ -41,27 +38,11 @@ public class DependencyManagerTest extends AbstractAppCenterTest {
 
     @Test
     public void setDependencyCallUsesInjectedHttpClient() throws Exception {
-        HttpClient mockHttpClient = new MockHttpClient();
+        HttpClient mockHttpClient = mock(HttpClient.class);
         DependencyManager.setDependencies(mockHttpClient);
         AppCenter.start(mApplication, DUMMY_APP_SECRET, (Class<? extends AppCenterService>) null);
 
         /* Verify that the channel was instantiated with the given HTTP client. */
         verifyNew(DefaultChannel.class).withArguments(any(Context.class), eq(DUMMY_APP_SECRET), any(LogSerializer.class), eq(mockHttpClient), any(Handler.class));
-    }
-
-    private static class MockHttpClient implements HttpClient {
-
-        @Override
-        public ServiceCall callAsync(String url, String method, Map<String, String> headers, CallTemplate callTemplate, ServiceCallback serviceCallback) {
-            return null;
-        }
-
-        @Override
-        public void reopen() {
-        }
-
-        @Override
-        public void close() {
-        }
     }
 }
