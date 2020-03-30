043e8a9769de48835ac2e9ce67d91d16afb0e7bb
Ivan Matkov <matkov.ivan@gmail.com>
2020-03-30 14:49:48 +0300

    Fix ProcessLifecycleOwner's copyright

--- sdk/appcenter/src/main/java/com/microsoft/appcenter/utils/ApplicationLifecycleListener.java
+++ sdk/appcenter/src/main/java/com/microsoft/appcenter/utils/ApplicationLifecycleListener.java
@@ -1,6 +1,18 @@
 /*
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License.
+ * Copyright (C) 2017 The Android Open Source Project
+ * Portions Copyright (c) Microsoft Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package com.microsoft.appcenter.utils;
@@ -21,8 +33,8 @@ import java.util.concurrent.CopyOnWriteArraySet;
  * It won't fire any events when there's a configuration change in a foregrounded app or if
  * user goes through activities inside a single application.
  *
- * The counters and timeouts approach is used from the ProcessLifecycleOwner class:
- * https://android.googlesource.com/platform/frameworks/support/+/refs/heads/androidx-master-dev/lifecycle/lifecycle-process/src/main/java/androidx/lifecycle/ProcessLifecycleOwner.java
+ * This is a simplified version of androidx.lifecycle.ProcessLifecycleOwner class:
+ * https://developer.android.com/reference/androidx/lifecycle/ProcessLifecycleOwner
  */
 public class ApplicationLifecycleListener implements ActivityLifecycleCallbacks {
 
