04e706e5dc8aef3dbe95e4cd462e1304a3135515
Anna Kocheshkova <v-annkoc@microsoft.com>
2020-08-04 12:18:01 +0300

    Cleanup

--- scripts/set-ndk-version.sh
+++ scripts/set-ndk-version.sh
@@ -6,6 +6,7 @@ function getProperty {
    PROP_VALUE=`cat $PROPERTY_FILE | sed 's/ //g' | grep "$PROP_KEY" | cut -d'=' -f2`
    echo $PROP_VALUE
 }
+
 PROJECT_DIR="$(dirname "$0")/.."
 PROPERTY_FILE="$HOME/Library/Android/sdk/ndk-bundle/source.properties"
 
@@ -17,7 +18,8 @@ GRADLE_FILE="$PROJECT_DIR/apps/sasquatch/build.gradle"
 if [ -z $VERSION ]; then
   echo "No NDK found in the default location. Proceeding..."
 else
+
+  # Insert ndkVersion = 'x.x.x' in the android section.
   NDK_VERSION_LINE="ndkVersion = '$VERSION'"
-  #sed -a '' 's/\(android { \).*/\1'$NDK_VERSION_LINE'/g' $file
   echo "$(sed "s/android {/android { \\`echo -e '\n\r\t'` $NDK_VERSION_LINE/g" "$GRADLE_FILE")" > $GRADLE_FILE
 fi
