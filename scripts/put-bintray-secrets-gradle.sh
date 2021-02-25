bc1984ea97fca53b1baf8f71744d78d0f4e18eec
Anastasia Kubova <v-ankubo@microsoft.com>
2021-02-25 12:39:28 +0300

    Update repository url

--- scripts/put-bintray-secrets-gradle.sh
+++ scripts/put-bintray-secrets-gradle.sh
@@ -2,8 +2,7 @@
 cat >> local.properties << EOL
 maven.user=${1:-$MAVEN_USER}
 maven.key=${2:-$MAVEN_KEY}
-maven.releasesRepoUrl=${3:-$MAVEN_RELEASE_REPO}
-maven.snapshotRepoUrl=${4:-$MAVEN_SNAPSHOT_REPO}
+maven.repoUrl=${3:-$MAVEN_REPO}
 maven.signingKeyId=${5:-$GDP_SIGNING_KEY_ID}
 maven.secretKeyPath=${6:-$GDP_KEY_SECRET_PATH}
 maven.publicKeyPassword=${7:-$GDP_KEY_PASSWORD}
