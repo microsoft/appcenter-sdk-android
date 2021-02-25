a1948db58613d798b4cc471aa1d444beb68b4d33
Anastasia Kubova <v-ankubo@microsoft.com>
2021-02-25 16:50:43 +0300

    Fix numbers in the script

--- scripts/put-bintray-secrets-gradle.sh
+++ scripts/put-bintray-secrets-gradle.sh
@@ -3,7 +3,7 @@ cat >> local.properties << EOL
 maven.user=${1:-$MAVEN_USER}
 maven.key=${2:-$MAVEN_KEY}
 maven.repoUrl=${3:-$MAVEN_REPO}
-maven.signingKeyId=${5:-$GDP_SIGNING_KEY_ID}
-maven.secretKeyPath=${6:-$GDP_KEY_SECRET_PATH}
-maven.publicKeyPassword=${7:-$GDP_KEY_PASSWORD}
+maven.signingKeyId=${4:-$GDP_SIGNING_KEY_ID}
+maven.secretKeyPath=${5:-$GDP_KEY_SECRET_PATH}
+maven.publicKeyPassword=${6:-$GDP_KEY_PASSWORD}
 EOL
