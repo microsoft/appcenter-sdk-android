d017b3394c8c3856f93d1e391e17260220103cba
Anastasia Kubova <v-ankubo@microsoft.com>
2021-02-25 12:09:54 +0300

    Update the release process

--- scripts/put-bintray-secrets-gradle.sh
+++ scripts/put-bintray-secrets-gradle.sh
@@ -3,7 +3,8 @@ cat >> local.properties << EOL
 maven.user=${1:-$MAVEN_USER}
 maven.key=${2:-$MAVEN_KEY}
 maven.releasesRepoUrl=${3:-$MAVEN_RELEASE_REPO}
-maven.signingKeyId=${4:-$GDP_SIGNING_KEY_ID}
-maven.publicKeyPassword=${5:-$GDP_KEY_PASSWORD}
+maven.snapshotRepoUrl=${4:-$MAVEN_SNAPSHOT_REPO}
+maven.signingKeyId=${5:-$GDP_SIGNING_KEY_ID}
 maven.secretKeyPath=${6:-$GDP_KEY_SECRET_PATH}
+maven.publicKeyPassword=${7:-$GDP_KEY_PASSWORD}
 EOL
