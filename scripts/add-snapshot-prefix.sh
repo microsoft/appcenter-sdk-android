933bed4c236e192adec56dee5e961d92de7b8fd1
Anastasia Kubova <v-ankubo@microsoft.com>
2021-03-04 09:32:53 +0300

    Resolve some feedback

--- scripts/add-snapshot-prefix.sh
+++ scripts/add-snapshot-prefix.sh
@@ -4,14 +4,10 @@
 # Usage: ./scripts/add-snapshot-prefix.sh
 
 # Compute new version
- gradleVersion=`egrep "versionName = '(.*)'" *.gradle | sed -E "s/^.*versionName = '(.*)'.*$/\1/"`
-baseGradleVersion=$(echo $gradleVersion| cut -d'-' -f 1)
+gradleVersion=`egrep "versionName = '(.*)'" *.gradle | sed -E "s/^.*versionName = '(.*)'.*$/\1/"`
+baseGradleVersion=`echo $gradleVersion | cut -d'-' -f 1`
 newVersion=$baseGradleVersion-"SNAPSHOT"
 
- if [[ "$USE_HASH" == "true" ]]
- then
-  newVersion+="+$(git rev-parse --short HEAD)"
-fi
 # Replace version in the file
 sed -E -i '' "s#(versionName = ')(.*)'#\1$newVersion'#" *.gradle
 # Print version on build logs
