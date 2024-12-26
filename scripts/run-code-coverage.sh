ba14087a121ec57611542fd50939b452d635df2f
Djordje <46656919+DordeDimitrijev@users.noreply.github.com>
2024-12-26 13:59:20 +0100

    Add arguments to Emulator boot process (#1749)

--- scripts/run-code-coverage.sh
+++ scripts/run-code-coverage.sh
@@ -4,7 +4,7 @@
 # VSTS will not execute next step until emulator killed
 # So we need to run tests in same step...
 export DYLD_LIBRARY_PATH="$ANDROID_HOME/emulator/lib64/qt/lib"
-$ANDROID_HOME/emulator/emulator -avd emulator -skin 768x1280 -no-window -gpu off &
+$ANDROID_HOME/emulator/emulator -avd emulator -skin 768x1280 -no-window -gpu off -no-metrics -no-audio -no-boot-anim -camera-back none -camera-front none &
 
 # Ensure Android Emulator has booted successfully before continuing
 EMU_BOOTED='unknown'
@@ -36,6 +36,10 @@ then
         git checkout -b $BRANCH
     fi
 fi
+
+# Wait for 30 seconds before running tests to prevent install exception
+echo "Waiting for 30 seconds..."
+sleep 30
 
 # Run tests with coverage
 if [ -z $1 ]
